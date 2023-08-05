ed4d1fe2f622851364e31c0dac4a00ce0875f6a4
jsum01 <djadjawltjr@gmail.com>
2023-08-05 14:48:06 +0900

    Complete Login page publishing

---
+++ src/component/Join.js
@@ -0,0 +1,8 @@
+export default function Join() {
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
--- src/component/Login.js
+++ src/component/Login.js
@@ -5,7 +5,7 @@ import styles from './Login.module.css'
 export function Login(props) {
   const navigate = useNavigate();
   return (
-    <div>
+    <div className={styles.body}>
       {/* loginPageContainer */}
       <div className={styles.loginPageContainer}>
         {/* Logo */}
@@ -17,8 +17,8 @@ export function Login(props) {
             onClick={() => { navigate("/") }}
           />
         </div>
-{/* 뭐하지 */}
-        <div className={styles.Container}>
+        {/* 뭐하지 */}
+        <div className={styles.container}>
           {/* loginContainer */}
           <div className={styles.loginContainer}>
             <div className={styles.inputContainer}>
@@ -30,24 +30,26 @@ export function Login(props) {
               <div>
                 <input className={styles.inputPW} placeholder={"패스워드를 입력하세요"} />
               </div>
-            </div>
-          {/* Login Button */}
-            <div className={styles.goLogin}>
-              로그인
+              <div className={styles.autoLoginCheckBox}>
+                <input type='checkbox' />로그인 상태 유지
+              </div>
+              {/* Login Button */}
+              <div className={styles.goLogin}>
+                로그인
+              </div>
             </div>
           </div>
-          {/* Find Id&PW Button | join Button */}
-          <div className={styles.autoLogin}>
 
-          </div>
-          <div className={styles.find_join}>
-            <input type='checkbox' placeholder={"자동 로그인"} />자동로그인
+
+
+          {/* Find Id&PW Button | join Button */}
             <div className={styles.findDiv}>
-              <div className={styles.findId}>아이디</div>
-              <div className={styles.findPW}>/ 비밀번호 찾기</div>
+              <div className={styles.findId}>아이디 찾기</div>
+              <div>|</div>
+              <div className={styles.findPW}>비밀번호 찾기</div>
+              <div>|</div>
+              <div className={styles.join}>회원가입</div>
             </div>
-            <div className={styles.join}>회원가입</div>
-          </div>
         </div>
       </div>
 
