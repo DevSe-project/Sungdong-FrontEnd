5534b8bcba5a184024c2f86572fd22ad143e8aa5
jsum01 <djadjawltjr@gmail.com>
2023-08-31 23:22:07 +0900

    성동니즈맞춤 기업용 고객센터로 변경위한 파일변동
    성동물산 측: 관리자 페이지에서만 고객 문의사항 확인 되도록.
    
    때문에 기존 질문게시판, 1:1채팅 관련 파일 전체 삭제.
    
    공지사항, 문의하기를 위한 컴포넌트 생성

---
+++ src/component/AboutUserService/Notice.js
@@ -0,0 +1,8 @@
+export function Notice() {
+
+  return(
+    <div>
+      공지사항
+    </div>
+  )
+}
--- src/component/Data/WritingsObj.js
+++
@@ -1,9 +0,0 @@
-export let WritingsObj = [
-  { 
-    id : 0,
-    title : '성동물산 Front-End 파이팅!',
-    writer : '엄지석',
-    date : `2023. 8. 22. 오후 8:23:55`,
-    content : '성동물산 프로젝트...제발 잘 완성해서 성공했으면 좋겠다.'
-  }
-]
