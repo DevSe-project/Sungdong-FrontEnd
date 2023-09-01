35b9f623baeb73d43b75eb60585faf8ee52813f9
jsum01 <djadjawltjr@gmail.com>
2023-09-01 22:38:29 +0900

    publishing user page & apply of  category logic
    1. 고객센터 페이지 카테고리 로직 추가
    2. 고객센터 퍼블리싱(간단한 레이아웃 구성 및 구상)
    3. 다른 컴포넌트와 배치 조정

--- src/component/AboutUserService/Notice.js
+++ src/component/AboutUserService/Notice.js
@@ -1,8 +1,19 @@
+import styles from './Notice.module.css';
+import { NoticeObj } from './Notice.module.css';
+
 export function Notice() {
 
   return(
-    <div>
-      공지사항
+    <div className={styles.noticeContainer}>
+      {/* 공지사항 */}
+      <div className={styles.post}>
+        공지사항
+      </div>
+      {/* 검색창 */}
+      <div>
+        <input />
+      </div>
+      {/* 글목록 */}
     </div>
   )
 }
