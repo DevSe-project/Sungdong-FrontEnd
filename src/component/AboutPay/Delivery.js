0009fe21ebd05443a4133cadf56a16fb4d3da5ba
taehun2123 <pyoneng_@naver.com>
2023-09-08 17:18:38 +0900

    Update : Delivery.js - 검색 기능 추가
    +filterOrderData 중 특정 단어 들어가 있으면 검색 됨.

--- src/component/AboutPay/Delivery.js
+++ src/component/AboutPay/Delivery.js
@@ -1,9 +1,39 @@
 import { useNavigate } from 'react-router-dom'
 import styles from './Delivery.module.css'
+import { useEffect, useState } from 'react';
 export function Delivery(props){
 
   //로그인 정보 불러오기
   const inLogin = JSON.parse(sessionStorage.getItem('saveLoginData'))
+  const filterOrderData = props.orderData && props.orderData.filter((item)=>item.userId === inLogin.id)
+  const [filterSearch, setFilterSearch] = useState("");
+  const [filteredItems, setFilteredItems] = useState([]);
+  const resultSearch = JSON.parse(sessionStorage.getItem('filterDelivery'))
+
+  useEffect(()=>{
+    if(resultSearch){
+      setFilterSearch(resultSearch)
+    }
+  },[resultSearch])
+
+  useEffect(() => {
+    if(filterOrderData){
+      if (filterSearch !== "") {
+        // 데이터에서 타이틀과 검색결과를 찾고
+        const filtered = filterOrderData.filter((item) =>
+          item.productName.includes(filterSearch))
+        // 목록 밑작업 해줌
+        const addCntList = filtered.map((item, index) => ({
+          ...item,
+          cnt: item.cnt ? item.cnt : 1,
+          finprice: item.finprice ? item.finprice : item.price,
+          listId: index,
+        }));
+        // 필터링 된 아이템 표시
+        setFilteredItems(addCntList);
+      }
+    }
+  }, [filterSearch, filterOrderData])
 
   const navigate = useNavigate();
   function detailOrder(item){
@@ -11,11 +41,12 @@ export function Delivery(props){
     navigate('/orderDetail');
   }
 
-  const filterOrderData = props.orderData && props.orderData.filter((item)=>item.userId === inLogin.id)
+
 
   return(
     <div className={styles.container}>
-      {props.orderData ? filterOrderData.map((item, key)=> 
+      {props.orderData ?
+      filteredItems.length > 0 ? filteredItems.map((item, key)=> 
       <div key={key} className={styles.deliveryList}>
         <div className={styles.orderDate}>
           <h4>{item.date} 주문</h4>
@@ -52,6 +83,44 @@ export function Delivery(props){
         </div>
       </div>
       )
+    :
+    filterOrderData.map((item, key) =>
+      <div key={key} className={styles.deliveryList}>
+      <div className={styles.orderDate}>
+        <h4>{item.date} 주문</h4>
+        <div onClick={()=>detailOrder(item)} className={styles.orderDetail}>
+          <span>주문 상세보기</span> 
+          <i className="far fa-chevron-right"></i>
+        </div>
+      </div>
+      <div className={styles.deliveryStyle}>
+        <div className={styles.deliveryNow}>
+          <div className={styles.deliveryNowMenu}>
+            <h5>{item.orderState === 0 ? '결제 대기' 
+            : item.orderState === 1 ? '결제 완료'
+            : item.orderState === 2 ? '배송 준비중'
+            : item.orderState === 3 ? '배송 중'
+            : item.orderState === 4 ? '배송 완료' 
+            : '누락된 상품(고객센터 문의)' }</h5>
+            <i style={{color: '#ccc'}} className="fas fa-trash-alt"></i>
+          </div>
+          <div className={styles.deliveryNowItem}>
+            <img className={styles.img} src='../../image/logo.jpeg' alt="주문상품"/>
+            <div className={styles.deliveryNowInformation}>
+              <span>{item.productName}{item.optionSelected && `(${item.optionSelected})`}, {item.cnt}개 </span>
+              <span>{item.discount 
+              ? item.finprice - item.finprice/100*item.discount
+              : item.finprice}원</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.deliveryMenu}>
+          <button onClick={()=> console.log(props.orderData)} className={styles.button}>배송 조회</button>
+          <button className={styles.button}>교환, 반품 신청</button>
+        </div>
+      </div>
+    </div>
+    )
     : 
     // 스켈레톤 처리
     <div className={styles.colskeleton}>
