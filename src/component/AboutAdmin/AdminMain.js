5d99c8908f0798fd143ee2335fac18f909cc9398
jsum01 <djadjawltjr@gmail.com>
2023-09-13 23:57:35 +0900

    [Make Visible] 관리자메인-보기 쉬운className으로 변경 & 주석추가

--- src/component/AboutAdmin/AdminMain.js
+++ src/component/AboutAdmin/AdminMain.js
@@ -1,23 +1,29 @@
 import styles from './AdminMain.module.css'
-export function AdminMain(){
-  return(
+export function AdminMain() {
+  return (
     <div className={styles.main}>
+      {/* 상품 관리 */}
+      <div className={styles.productManage}>
+
+      </div>
+      {/* 배송 관리 */}
       <div className={styles.deliveryManage}>
 
       </div>
-      <div className={styles.claimManage}>
-
-      </div>
+      {/* 수익 관리 */}
       <div className={styles.benefitManage}>
 
       </div>
-      <div className={styles.customerManage}>
+      {/* 고객요청 관리 */}
+      <div className={styles.claimManage}>
 
       </div>
-      <div className={styles.reviewManage}>
-        
+      {/* 고객 관리 */}
+      <div className={styles.userManage}>
+
       </div>
-      <div className={styles.productManage}>
+      {/* 리뷰 관리 */}
+      <div className={styles.reviewManage}>
 
       </div>
     </div>
