4fa23b33eb203eb4c76565c46bf6d20262bd2234
taehun2123 <pyoneng_@naver.com>
2023-10-11 12:27:04 +0900

    Update : 필터 컨테이너 스타일 다소 수정 - 형태 완성

--- src/component/AboutAdmin/AdminProductFilter.js
+++ src/component/AboutAdmin/AdminProductFilter.js
@@ -2,7 +2,10 @@ import styles from './AdminProductFilter.module.css'
 export function AdminProductFilter(){
   const filterList = [
     { label : '검색어', content : <input/>},
-    { label : '검색어', content : <input/>},
+    { label : '판매상태', content : <input/>},
+    { label : '카테고리', content : <input/>},
+    { label : '결제여부', content : <input/>},
+    { label : '기간', content : <input/>},
   ]
   return(
     <div className={styles.main}>
