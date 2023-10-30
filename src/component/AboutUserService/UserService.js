5c6d64573502824442ed109e19b951d569c0c8e8
jsum01 <djadjawltjr@gmail.com>
2023-10-30 23:03:31 +0900

    Remove : 고객센터 사이드바 삭제

--- src/component/AboutUserService/UserService.js
+++ src/component/AboutUserService/UserService.js
@@ -15,29 +15,19 @@ export function UserService(props) {
             serviceTitle : '문의하기',
             link : '/userservice/ask    ',
         },
-        
+
     ]
     
     const navigate = useNavigate();
 
     return(
         <div className={styles.body}>
+            {/* TopBanner */}
             <TopBanner menuOnClick={props.menuOnClick} menu_dynamicStyle={props.menu_dynamicStyle} data={props.data} setData={props.setData} categoryData={props.categoryData} setCategoryData={props.setCategoryData} login={props.login} setLogin={props.setLogin} iconHovered={props.iconHovered} iconMouseEnter={props.iconMouseEnter} iconMouseLeave={props.iconMouseLeave} icon_dynamicStyle={props.icon_dynamicStyle} text_dynamicStyle={props.text_dynamicStyle} category_dynamicStyle={props.category_dynamicStyle} iconOnClick={props.iconOnClick} />
-            <div className={styles.mainScreen}> {/*메인화면 가로정렬*/}
-                <div className={styles.sideBar}> {/*사이드바 세로정렬*/}
-                    <div className={styles.title} onClick={ () => {navigate('/userservice/')} }> 고객센터 </div>
-                    {/* 목록 */}
-                    <div className={styles.sideContents}>
-                        { serviceList.map( (serviceList, index) => {
-                            return <div className={styles.each_sideContents} key={index} onClick={ () => { 
-                                navigate(`${serviceList.link}`);
-                                } }>
-                                {serviceList.serviceTitle}
-                            </div>  
-                        } ) }
-                    </div>
-                </div>
-                {/* 해당 게시판을 보여줄 구멍 */}
+            {/*메인화면 가로정렬*/}
+            <div className={styles.mainScreen}>
+                
+                {/* 우측 : 해당 게시판을 보여줄 구멍 */}
                 <div className={styles.mainComponent}>
                     {/* 처음 고객센터 인사말을 보여주고 사이드바의 개체를 선택하면 이동하는 식으로 구현할 예정 */}
                     <Outlet />
