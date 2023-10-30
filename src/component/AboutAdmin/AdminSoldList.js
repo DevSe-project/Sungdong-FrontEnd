aa59a083e97d2ec9a633cb9564b9b30b2c598b6a
taehun2123 <pyoneng_@naver.com>
2023-10-30 17:25:13 +0900

    Update : 결제완료 주문 리스트 간소화
    Modal창을 이용하여 주문자 정보 출력

--- src/component/AboutAdmin/AdminSoldList.js
+++ src/component/AboutAdmin/AdminSoldList.js
@@ -5,26 +5,16 @@ import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import React from 'react';
 import { AdminSoldFilter } from './AdminSoldFilter';
+import AdminSoldModal from './AdminSoldModal';
 export function AdminSoldList(props){
   // 필터된 항목을 저장할 상태 변수
   const [filteredItems, setFilteredItems] = useState([]);
 
-  //Td 선택시 Modal State 변수
-  const [selectedData, setSelectedData] = useState(null);
+  const [modal, setModal] = useState(false);
 
   const navigate = useNavigate();
 
   const filteredOrderData = props.orderData && props.orderData.filter((item) => item.orderState === 1);
-
-  // 아이템 클릭 핸들러
-  const handleItemClick = (itemId) => {
-    if (selectedData === itemId) {
-      // 이미 선택된 아이템을 클릭한 경우 모달을 닫음
-      setSelectedData(null);
-    } else {
-      setSelectedData(itemId);
-    }
-  };
     
   // 게시물 데이터와 페이지 번호 상태 관리    
   const [currentPage, setCurrentPage] = useState(1);
@@ -68,13 +58,14 @@ export function AdminSoldList(props){
                 <tr>
                   <th><input type='checkbox'/></th>
                   <th>이미지</th>
-                  <th>상품코드</th>
-                  <th>주문번호</th>
-                  <th>상품명</th>
-                  <th>주문량</th>
-                  <th>공급가</th>
-                  <th style={{fontWeight: '650'}}>주문가</th>
-                  <th>주문정보</th>
+                  <th style={{width:'10%'}}>상품코드</th>
+                  <th style={{width:'10%'}}>주문번호</th>
+                  <th style={{width:'10%'}}>상품명</th>
+                  <th style={{width:'10%'}}>옵션</th>
+                  <th style={{width:'10%'}}>주문량</th>
+                  <th style={{width:'10%'}}>공급가</th>
+                  <th style={{width:'10%', fontWeight: '650'}}>주문가</th>
+                  <th style={{width:'10%', fontWeight: '650'}}>주문자 정보</th>
                 </tr>
               </thead>
               <tbody>
@@ -88,9 +79,10 @@ export function AdminSoldList(props){
                     <td>
                       {item.orderId}
                     </td>
-                    <td className={styles.detailView} onClick={()=>handleItemClick(item.id)}>
+                    <td>
                       <h5 style={{fontSize: '1.1em', fontWeight: '550'}}>{item.productName}</h5>
                     </td>
+                    <td>{item.optionSelected}</td>
                     <td>{item.cnt}</td>
                     <td>\{item.price.toLocaleString()}</td>
                     <td style={{fontWeight: '750'}}>
@@ -102,119 +94,11 @@ export function AdminSoldList(props){
                     </td>
                     <td 
                       className={styles.detailView}
-                      onClick={()=>handleItemClick(item.id)}>
-                      더보기&nbsp;{selectedData === item.id  
-                      ? <i className="fa-sharp fa-solid fa-caret-up"></i>
-                      : <i className="fa-sharp fa-solid fa-caret-down"></i>}&nbsp;
+                      onClick={()=>setModal(!modal)}>
+                      보기
                     </td>
                   </tr>
-                  {/* 모달 */}
-                  {selectedData === item.id && (
-                  <tr>
-                    <td colSpan="9">
-                      <table className={styles.colTable}>
-                        <thead style={{ backgroundColor: 'white', color: 'black', boxShadow: '0 1px 2px rgba(0, 0, 0, 0.6)'}}>
-                          <tr>
-                            <th style={{width: '10%'}}>
-                              배송 속성
-                            </th>
-                            <th style={{width: '10%'}}>
-                              배송사
-                            </th>
-                            {item.delivery.deliveryDate
-                            &&
-                            <th style={{width: '15%'}}>
-                              배송 희망일
-                            </th>
-                            }
-                            <th style={{width: '10%'}}>
-                              옵션
-                            </th>
-                            <th style={{width: '15%'}}>
-                              받을 배송지
-                            </th>
-                            <th style={{width: '10%'}}>
-                              결제 방법
-                            </th>
-                            <th style={{width: '10%'}}>
-                              증빙 서류
-                            </th>
-                            {item.order.moneyReceipt &&
-                            <th style={{width: '10%'}}>
-                              발급 방식
-                            </th>
-                            }
-                            {
-                              item.order.transAction === "명세서출력"
-                              && 
-                              <th style={{width: '10%'}}>
-                                팩스
-                              </th>
-                            }
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                            <td>
-                              {item.delivery.deliveryType}
-                            </td>
-                            <td>
-                              {item.delivery.deliveryType === '일반택배'
-                              ? "대한통운"
-                              : item.delivery.deliveryType === '화물'
-                              ? item.delivery.deliverySelect
-                              : "없음"
-                              }
-                            </td>
-                            {item.delivery.deliveryDate
-                            && <td>{item.delivery.deliveryDate}</td>
-                            }
-                            <td>
-                              {item.optionSelected 
-                              ?            
-                              item.optionSelected
-                              :
-                              <span>옵션없음</span>
-                              }
-                            </td>
-                            <td>
-                            {
-                            item.delivery.address 
-                            ? item.delivery.address.address 
-                            ? `${item.delivery.address.address.roadAddress} 
-                            (${item.delivery.address.address.bname}, 
-                              ${item.delivery.address.address.buildingName 
-                            ? item.delivery.address.address.buildingName
-                            : item.delivery.address.address.jibunAddress})
-                            ${item.delivery.address.addressDetail}` 
-                            : item.delivery.address.addressDetail
-                            : null
-                            }
-                            </td>
-                            <td>
-                              {item.order.payRoute}
-                            </td>
-                            <td>
-                              {item.order.moneyReceipt}
-                            </td>
-                            {
-                            item.order.moneyReceipt 
-                            &&
-                            <td>
-                              {item.order.transAction}
-                            </td>
-                            }
-                            {
-                              item.order.transAction === "명세서출력"
-                              &&
-                              <td>{item.order.fax}</td>
-                            }
-                          </tr>
-                        </tbody>
-                      </table>
-                    </td>
-                  </tr>
-                  )}
+                  {modal && <AdminSoldModal setModal={setModal} item={item}/>}
                   </React.Fragment>
                   ))
                 : <tr><td>로딩중</td></tr>
---
+++ src/component/AboutAdmin/AdminSoldModal.js
@@ -0,0 +1,140 @@
+import { React, useEffect, useState } from 'react';
+import styles from './AdminSoldModal.module.css';
+import { useNavigate } from 'react-router-dom';
+
+export default function AdminSoldModal(props) {
+
+
+
+  const navigate = useNavigate();
+
+  // esc키를 누르면 모달창 닫기.
+  useEffect(() => {
+    const exit_esc = (event) => {
+      if (event.key === 'Escape') {
+        props.setModal(false); // "Esc" 키 누를 때 모달 닫기 함수 호출
+      }
+    };
+
+    window.addEventListener('keydown', exit_esc);
+
+    return () => {
+      window.removeEventListener('keydown', exit_esc);
+    };
+  }, [props.setModal]);
+
+  const orderInputValue = [
+    { 
+      id : 0, 
+      title : '성함', 
+      value : props.item.order && props.item.order.name,
+    },
+    { 
+      id : 1, 
+      title : '전화번호', 
+      value : props.item.order && props.item.order.tel,
+    },
+    { 
+      id : 2, 
+      title : '주소', 
+      value : 
+      props.item.delivery &&
+      props.item.delivery.address &&
+      props.item.delivery.address.address 
+      ?
+      `${props.item.delivery.address.address.roadAddress} 
+      (${props.item.delivery.address.address.bname}, 
+        ${props.item.delivery.address.address.buildingName 
+      ? props.item.delivery.address.address.buildingName
+      : props.item.delivery.address.address.jibunAddress})
+      ${props.item.delivery.address.addressDetail}` 
+      : '',
+    },
+    { 
+      id : 3, 
+      title : '배송방식', 
+      value : props.item.delivery &&
+      props.item.delivery.deliveryType &&
+      props.item.delivery.deliveryType === '화물'
+
+      ? props.item.delivery.deliverySelect === 'kr.daesin' 
+      ? `${props.item.delivery && props.item.delivery.deliveryType} (배송 업체 : 대신화물)` 
+      : `${props.item.delivery && props.item.delivery.deliveryType} (배송 업체 : 경동화물)`
+
+      : props.item.delivery &&
+      props.item.delivery.deliveryType &&
+      props.item.delivery.deliveryType === '성동택배'
+
+      ? `${props.item.delivery && props.item.delivery.deliveryType} 
+      (배송 예정일 : ${props.item.delivery && props.item.delivery.deliveryDate})`
+      : props.item.delivery && props.item.delivery.deliveryType === '일반택배'
+      ? `${props.item.delivery.deliveryType} (배송사 : 대한통운)`
+      : '없음'
+    },
+    { 
+      id : 4, 
+      title : '배송 메세지', 
+      value : props.item.delivery && props.item.delivery.deliveryMessage ? props.item.delivery.deliveryMessage : '없음',
+    },
+    { 
+      id : 5, 
+      title : '성동 메세지', 
+      value : props.item.order && props.item.order.smtMessage ? props.item.order.smtMessage : '없음',
+    },
+    { 
+      id : 6, 
+      title : '결제 방법', 
+      value : props.item.order && props.item.order.payRoute,
+    },
+    {
+      id : 7, 
+      title : '증빙 서류 발급', 
+      value : props.item.order && props.item.order.moneyReceipt,
+    },
+    {
+      id: 8,
+      title : '명세서',
+      value : props.item.order
+      && props.item.order.moneyReceipt 
+      && props.item.order.transAction
+      ? props.item.order.transAction === '명세서출력'
+      ? `${props.item.order.transAction} (Fax 번호 : ${props.item.order.fax})`
+      : props.item.order.transAction
+      : '발행안함'
+    }
+  ];
+
+  return (
+    <div className={styles.modalOverlay}>
+      <div className={styles.modalContainer}>
+        {/* Exit Button */}
+        <div className={styles.exitButton}>
+          <span onClick={() => { props.setModal(false) }}>
+            <i className="fas fa-times"></i>
+          </span>
+        </div>
+        {/* Title */}
+        <div className={styles.modalContent}>
+          <div className={styles.titleBox}>
+            <div className={styles.title}>
+              주문자 정보
+            </div>
+          </div>
+        </div>
+        {/* 주문자 정보 */}
+        <div className={styles.codeContainer}>
+          {orderInputValue.map((item) => 
+          <div className={styles.codeInputContainer}>
+            <div className={styles.label}>
+              {item.title}
+            </div>
+            <div className={styles.input}>
+              {item.value}
+            </div>
+          </div>
+         )}
+        </div>
+      </div>
+    </div>
+  );
+}
