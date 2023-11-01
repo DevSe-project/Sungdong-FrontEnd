28da4df9cc123c72c5a2cd19c784c081977e2092
jsum01 <djadjawltjr@gmail.com>
2023-11-01 22:44:27 +0900

    Update : 공지사항 퍼블리싱 완료(검색엔진 X)

--- src/component/AboutUserService/Notice.js
+++ src/component/AboutUserService/Notice.js
@@ -7,73 +7,77 @@ import { NoticeObj } from '../Data/NoticeObj';
 export function Notice(props) {
   const [list, setList] = useState(NoticeObj);
   const [isModal, setIsModal] = useState(false);
-  // 글 클릭 > 해당 글의 상세페이지로 이동
+  const [selectedItemIndex, setSelectedItemIndex] = useState(null);
 
-  // modal escape
-  const onClose = (event) => {
-    if(event.key === 'Escape' || event.key === 'esc')
-      setIsModal(false);
+  // 글 위치에서 enter를 누르면 해당 페이지가 열림
+  const onOpen = (event, index) => {
+    if (event.key === 'Enter') {
+      setIsModal(true);
+      setSelectedItemIndex(index);
+    }
   }
 
   return (
     <div className={styles.body}>
-    {/* TopBanner */}
-    <TopBanner menuOnClick={props.menuOnClick} menu_dynamicStyle={props.menu_dynamicStyle} data={props.data} setData={props.setData} categoryData={props.categoryData} setCategoryData={props.setCategoryData} login={props.login} setLogin={props.setLogin} iconHovered={props.iconHovered} iconMouseEnter={props.iconMouseEnter} iconMouseLeave={props.iconMouseLeave} icon_dynamicStyle={props.icon_dynamicStyle} text_dynamicStyle={props.text_dynamicStyle} category_dynamicStyle={props.category_dynamicStyle} iconOnClick={props.iconOnClick} />
-    
-    <div className={styles.noticeContainer}>
-      {/* --------------header-------------- */}
-      <div className={styles.header}>
-        <h1 className={styles.title}>공지사항</h1>
-        <div className={styles.searchContainer}>
-          <label className={styles.search_label} htmlFor="search">검색:</label>
-          <input
-            className={styles.search_input}
-            type="text"
-            id="search"
-            placeholder="검색어를 입력하세요"
-          />
-        </div>
-      </div>
-      {/* --------------listContainer-------------- */}
-      <div className={styles.listContainer}>
-        <table className={styles.listTable}>
-          <thead>
-            <tr>
-              <th>구분</th>
-              <th>제목</th>
-              <th>작성자</th>
-              <th>작성일</th>
-              <th>조회수</th>
-            </tr>
-          </thead>
-          <tbody>
-            {list.map((item, index) => (
-              <tr key={index} 
-                className={styles.row}
-                onClick={() => { setIsModal(!isModal) }}>
-                <td>{item.id}</td>
-                <td>{item.title}</td>
-                <td>{item.writer}</td>
-                <td>{item.date}</td>
-                <td>{item.view}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      {/* --------------Detail-Modal-------------- */}
-      {isModal ? 
-        <div className={styles.modalOverlay}>
-          <div className={styles.modalContainer}>
-            <NoticeDetail list={list} 
-              setList={setList} 
-              isModal={isModal}
-              setIsModal={setIsModal}
-              onClose={onClose}/>
+      {/* TopBanner */}
+      <TopBanner menuOnClick={props.menuOnClick} menu_dynamicStyle={props.menu_dynamicStyle} data={props.data} setData={props.setData} categoryData={props.categoryData} setCategoryData={props.setCategoryData} login={props.login} setLogin={props.setLogin} iconHovered={props.iconHovered} iconMouseEnter={props.iconMouseEnter} iconMouseLeave={props.iconMouseLeave} icon_dynamicStyle={props.icon_dynamicStyle} text_dynamicStyle={props.text_dynamicStyle} category_dynamicStyle={props.category_dynamicStyle} iconOnClick={props.iconOnClick} />
+
+      <div className={styles.noticeContainer}>
+        {/* --------------header-------------- */}
+        <div className={styles.header}>
+          <h1 className={styles.title}>공지사항</h1>
+          <div className={styles.searchContainer}>
+            <label className={styles.search_label} htmlFor="search">검색:</label>
+            <input
+              className={styles.search_input}
+              type="text"
+              id="search"
+              placeholder="검색어를 입력하세요"
+            />
           </div>
         </div>
-      : null}
-    </div>
+        {/* --------------listContainer-------------- */}
+        <div className={styles.listContainer}>
+          <table className={styles.listTable}>
+            <thead>
+              <tr>
+                <th>구분</th>
+                <th>제목</th>
+                <th>작성자</th>
+                <th>작성일</th>
+              </tr>
+            </thead>
+            <tbody>
+              {list.map((item, index) => (
+                <tr key={index}
+                  className={styles.row}
+                  onClick={() => { setIsModal(true); setSelectedItemIndex(index); }}
+                  onKeyPress={(event) => onOpen(event, index)} // Enter 키 이벤트 처리
+                  tabIndex={0} // 이렇게 하면 포커스를 받을 수 있게 됩니다
+                >
+                  <td>{item.id}</td>
+                  <td>{item.title}</td>
+                  <td>{item.writer}</td>
+                  <td>{item.date}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        {/* --------------Detail-Modal-------------- */}
+        {isModal ?
+          <div className={styles.modalOverlay}>
+            <NoticeDetail
+              list={list}
+              setList={setList}
+              isModal={isModal}
+              setIsModal={setIsModal}
+              onClose={() => setSelectedItemIndex(null)} // 모달이 닫힐 때 selectedItemIndex 초기화
+              selectedItemIndex={selectedItemIndex} // 선택된 항목 인덱스 전달
+            />
+          </div>
+          : null}
+      </div>
     </div>
   );
 }
--- src/component/AboutUserService/NoticeDetail.js
+++ src/component/AboutUserService/NoticeDetail.js
@@ -2,25 +2,56 @@ import styles from './NoticeDetail.module.css';
 import { useEffect } from 'react';
 
 export default function NoticeDetail(props) {
-    
-// esc키를 누르면 모달창 닫기.
-useEffect(() => {
-    const onClose = (event) => {
-        if (event.key === 'Escape') {
-            props.setIsModal(false); // "Esc" 키 누를 때 모달 닫기 함수 호출
-        }
-    };
 
-    window.addEventListener('keydown', onClose);
+    // esc키를 누르면 모달창 닫기.
+    useEffect(() => {
+        const onClose = (event) => {
+            if (event.key === 'Escape') {
+                props.setIsModal(false); // "Esc" 키 누를 때 모달 닫기 함수 호출
+            }
+        };
 
-    return () => {
-        window.removeEventListener('keydown', onClose);
-    };
-}, );
+        window.addEventListener('keydown', onClose);
 
-    return (
-        <div className={styles.detailContainer}>
-            제목 : {props.list[0].title}
-        </div>
-    )
+        return () => {
+            window.removeEventListener('keydown', onClose);
+        };
+    }, []);
+
+    // 선택된 index가 있을 때만 렌더링
+    if (props.selectedItemIndex !== null && props.selectedItemIndex !== undefined) {
+        return (
+            <div className={styles.modalContainer}>
+                {/* 종료 버튼 */}
+                <div className={styles.closeButton}>
+                    <span onClick={() => props.setIsModal(false)}>
+                        <i className="fas fa-times"></i>
+                    </span>
+                </div>
+                {/* 본문 컨테이너 */}
+                <div className={styles.contentContainer}>
+                    {/* 제목 */}
+                    <div className={styles.title}>
+                        {props.list[props.selectedItemIndex].title}
+                    </div>
+                    {/* 작성일과 작성자 */}
+                    <div className={styles.details}>
+                        <div className={styles.date}>
+                            작성일: {props.list[props.selectedItemIndex].date}
+                        </div>
+                        <div className={styles.writer}>
+                            작성자: {props.list[props.selectedItemIndex].writer}
+                        </div>
+                    </div>
+                    {/* 글 내용 */}
+                    <div className={styles.content}>
+                        {props.list[props.selectedItemIndex].contents}
+                    </div>
+                </div>
+            </div>
+        );
+    } else {
+        // selectedItemIndex가 없을 때 렌더링할 내용
+        return null;
+    }
 }
