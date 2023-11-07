5af09aee504c884537e4a939290c18288b7449ee
jsum01 <djadjawltjr@gmail.com>
2023-11-07 19:44:04 +0900

    Update : DataStore  주석작업

--- src/store/DataStore.js
+++ src/store/DataStore.js
@@ -1,52 +1,66 @@
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
 export const useDataStore = create((set)=>({
+  // 상품
   data : [],
   setData : (input) =>
    set({
      data : input
      }),
+  // 주문
   orderData: [],
   setOrderData : (input) =>
   set( (prev) => ({
     orderData : input
     })),
+  // 카테고리
   categoryData: [],
   setCategoryData : (input) =>
   set( (prev) => ({
     categoryData : input
     })),
+  // 고객정보
   userData: [],
   setUserData : (input) =>
   set( (prev) => ({
     userData : input
     })),
+  // 오늘의 주제
   todayTopicData: [],
   setTodayTopicData : (input) =>
   set( (prev) => ({
     todayTopicData : input
-    })),
+    }))
 }))
 
 export const useListStore = create((set)=>({
+  // 찜
   wishList : [],
   setWishList : (val) =>
    set( (prev) => ({
      wishList : val
     })),
+  // 주문
   orderList : [],
   setOrderList : (val) =>
     set( (state) => ({
       orderList : val 
     })),
+  // 장바구니
   basketList : [],
   setBasketList : (val) =>
    set( (state) => ({
      basketList : val 
     })),
+  // 공지사항
   postList : [],
   setPostList : (val) =>
     set( (state) => ({
       postList : val 
     }))
+}))
+
+
+export const useNoticeStroe = create((set) => ({
+
 }))
