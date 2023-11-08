ce449d41575f28aa5ab8b6179b9218ddc783b69d
jsum01 <djadjawltjr@gmail.com>
2023-11-08 23:43:39 +0900

    Update : 문의하기 초안

--- src/component/AboutUserService/Contact.js
+++ src/component/AboutUserService/Contact.js
@@ -1,8 +1,41 @@
-export function Contact() {
-  
-  return(
+import styles from './Contact.module.css';
+
+import { TopBanner } from "../TemplateLayout/AboutHeader/TopBanner"; // 최상단 배너
+import { Footer } from "../TemplateLayout/AboutFooter/Footer"; // 최하단 배너
+import { useDataStore } from "../../Store/DataStore"; // zustand스토어 import
+import { MenuData } from "../TemplateLayout/AboutMenuData/MenuData";
+
+
+export function Contact(props) {
+
+  const { data, setData,
+    categoryData, setCategoryData,
+    login, setLogin
+  } = useDataStore();
+
+  //로그인 정보 불러오기
+  return (
     <div>
-      문의하기
+      {/* TOP */}
+      <TopBanner iconHovered={props.iconHovered}
+        iconMouseEnter={props.iconMouseEnter} iconMouseLeave={props.iconMouseLeave}
+        icon_dynamicStyle={props.icon_dynamicStyle} text_dynamicStyle={props.text_dynamicStyle}
+        category_dynamicStyle={props.category_dynamicStyle} iconOnClick={props.iconOnClick}
+        menuOnClick={props.menuOnClick} menu_dynamicStyle={props.menu_dynamicStyle} />
+
+      {/* MIDDLE */}
+      <div className={styles.middle}>
+        {/* 메뉴바 */}
+        <MenuData />
+        
+        {/* contentsContainer */}
+        <div>
+          문의처 - 052)269-1840
+        </div>
+      </div>
+
+      {/* BOTTOM */}
+      <Footer />
     </div>
   )
 }
