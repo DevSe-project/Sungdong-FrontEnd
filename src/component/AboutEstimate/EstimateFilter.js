32416ae24df9baa036334f18658564262bffdf9a
taehun2123 <pyoneng_@naver.com>
2023-11-24 19:58:11 +0900

    Update : EstimateManager 퍼블리싱 완료

---
+++ src/component/AboutEstimate/EstimateFilter.js
@@ -0,0 +1,90 @@
+import { useState } from 'react';
+import styles from './Filter.module.css'
+import { addMonths, subMonths, format } from 'date-fns';
+
+export function EstimateFilter(){
+  const filterList = [
+    { label : '조회일자', content : DateFilter()},
+    { label : '출력', content : detailSearch()},
+  ]
+  return(
+    <div style={{width: '100%'}}>
+      <form className={styles.main}>
+        <div style={{ width: '90%', textAlign: 'left', padding: '1.5em', borderBottom: '1px solid lightgray'}}>
+          <h4 style={{fontSize: '1.2em', fontWeight: '650'}}>필터</h4>
+        </div>
+        {filterList.map((item) => (
+        <div className={styles.container}>
+          <div className={styles.label}>
+            {item.label}
+          </div>
+          <div className={styles.content}>
+            {item.content}
+          </div>
+        </div>
+        ))}
+        <div style={{display: 'flex', gap: '0.5em'}}>
+          <input className={styles.button} type='submit' value='검색'/>
+          <input className={styles.button} type='reset'/>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+function DateFilter(){
+  const today = new Date();
+  const [startDate, setStartDate] = useState(today);
+  const [endDate, setEndDate] = useState(addMonths(startDate, 1));
+  const handleMonthChange = (event) => {
+    setStartDate(new Date(2023,event.target.value,1));
+    setEndDate(new Date(2023,event.target.value,31));
+  };
+
+  const dateList = () => {
+    return Array.from({ length: 12 }, (_, index) => (
+      <option key={index} value={index}>{index + 1}월</option>
+    ));
+  };
+
+  const handleStartDateChange = (event) => {
+    const newStartDate = new Date(event.target.value);
+    setStartDate(newStartDate);
+  };
+  
+  const handleEndDateChange = (event) => {
+    const newStartDate = new Date(event.target.value);
+    setEndDate(newStartDate);
+  };
+  return(
+    <div style={{ display: 'flex', gap: '1em' }}>
+      <select onChange={(e)=>handleMonthChange(e)}>
+        {dateList()}
+      </select>
+      <div>
+        <input 
+        className={styles.button}
+        type='date' 
+        value={format(startDate, 'yyyy-MM-dd')}
+        onChange={(e)=>handleStartDateChange(e)}
+        />
+        &nbsp;~&nbsp;
+        <input 
+        className={styles.button}
+        type='date' 
+        value={format(endDate, 'yyyy-MM-dd')}
+        onChange={(e)=>handleEndDateChange(e)}
+        />
+      </div>
+    </div>
+  )
+}
+
+function detailSearch(){
+  return(
+    <div style={{display: 'flex', gap: '1em'}}>
+      <button className={styles.button}>인쇄</button>
+      <button className={styles.button}>액셀</button>
+    </div>
+  )
+}
--- src/component/AboutEstimate/EstimateManager.js
+++ src/component/AboutEstimate/EstimateManager.js
@@ -1,6 +1,26 @@
+import { useState } from 'react';
+import { EstimateFilter } from './EstimateFilter';
 import styles from './Table.module.css';
+import { useQuery } from '@tanstack/react-query';
 
 export function EstimateManager(){
+  const { data, isLoading, isError, error } = useQuery({queryKey: ['data']});
+  // 게시물 데이터와 페이지 번호 상태 관리    
+  const [currentPage, setCurrentPage] = useState(1);
+  //현재 페이지에 해당하는 게시물 목록 가져오기
+  const getCurrentPagePosts = () => {
+    const startIndex = (currentPage - 1) * 5; // 한 페이지에 5개씩 표시
+    return data.length > 0 
+    ? data.slice(startIndex, startIndex + 5)
+    : data.slice(startIndex, startIndex + 5)
+    
+  };
+  if (isLoading) {
+    return <p>Loading..</p>;
+  }
+  if (isError) {
+    return <p>에러 : {error.message}</p>;
+  }
   return(
   <div>
     <div style={{width:'90%'}}>
@@ -8,6 +28,8 @@ export function EstimateManager(){
       <div className={styles.head}>
         <h1><i className="fa-solid fa-heart"/> 견적함</h1>
       </div>
+      {/* 필터 */}
+      <EstimateFilter/>
       {/* 테이블 */}
       <div className={styles.tablebody}>
         <table className={styles.table}>
@@ -19,6 +41,7 @@ export function EstimateManager(){
               <th>품명 및 규격</th>
               <th>금액</th>
               <th>주문하기</th>
+              <th>견적서 엑셀</th>
               <th>작성자</th>
               <th><input type='checkbox'></input></th>
             </tr>
@@ -31,14 +54,39 @@ export function EstimateManager(){
               <td>품명 및 규격</td>
               <td>금액</td>
               <td>주문하기</td>
+              <td>견적서 엑셀</td>
               <td>작성자</td>
               <td><button className={styles.button}>삭제</button></td>
             </tr>
           </tbody>
         </table>
       </div>
-      <div>
-        <button className={styles.submitButton}>견적하기</button>
+      <div className={styles.buttonContainer}>
+        {/* 이전 페이지 */}
+        <button
+          className={styles.pageButton} 
+          onClick={()=> {
+            if(currentPage !== 1){
+              setCurrentPage(currentPage - 1)
+            } else {
+              alert("해당 페이지가 가장 첫 페이지 입니다.")
+        }}}>
+        <i className="far fa-angle-left"/>
+        </button>
+        <div className={styles.pageButton}>
+          {currentPage}
+        </div>
+          {/* 다음 페이지 */}
+        <button
+        className={styles.pageButton}
+        onClick={()=> {
+          if(data.length > 5){
+            setCurrentPage(currentPage + 1)
+          } else {
+            alert("다음 페이지가 없습니다.")
+          }}}>
+            <i className="far fa-angle-right"/>
+        </button>
       </div>
     </div>
   </div>
