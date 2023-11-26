d4b10fbbe7953e51826569e1067ea43bb9997fe6
jsum01 <djadjawltjr@gmail.com>
2023-11-26 22:21:55 +0900

    Update 고객관리검색필터: 퍼블리싱(데모)

--- src/component/AboutAdmin/Users/FilterSearch_User.js
+++ src/component/AboutAdmin/Users/FilterSearch_User.js
@@ -1,21 +1,57 @@
 import React from 'react';
 import styles from './FiterSearch_User.module.css';
-import { db } from '../../../firebase';
-
 
 export default function FilterSearch_User() {
     return (
-        <div>
+        <div className={styles.searchbar}>
             {/* Filter options */}
-            <input className={styles.input} type="text" placeholder="기업명" />
-            <input className={styles.input} type="text" placeholder="대표명" />
-            <input className={styles.input} type="text" placeholder="사업자등록번호" />
-            <select className={styles.select}>
-                <option value="customerType">고객유형</option>
-            </select>
-            <select className={styles.select}>
-                <option value="grade">등급</option>
-            </select>
+            <div className={styles.searchRow}>
+                <div className={styles.searchGroup}>
+                    <label htmlFor="bizName">기업명</label>
+                    <input
+                        id="bizname"
+                        className={styles.input}
+                        type="text"
+                        placeholder="기업명"
+                    />
+                </div>
+
+                <div className={styles.searchGroup}>
+                    <label htmlFor="ceoName">대표명</label>
+                    <input
+                        id="ceoName"
+                        className={styles.input}
+                        type="text"
+                        placeholder="대표명"
+                    />
+                </div>
+
+                <div className={styles.searchGroup}>
+                    <label htmlFor="bizNum">사업자등록번호</label>
+                    <input
+                        id="bizNum"
+                        className={styles.input}
+                        type="text"
+                        placeholder="사업자등록번호"
+                    />
+                </div>
+            </div>
+
+            <div className={styles.searchRow}>
+                <div className={styles.searchGroup}>
+                    <label htmlFor="customerType">고객유형</label>
+                    <select className={styles.select} id="customerType">
+                        <option value="customerType">고객유형</option>
+                    </select>
+                </div>
+
+                <div className={styles.searchGroup}>
+                    <label htmlFor="grade">등급</label>
+                    <select className={styles.select} id="grade">
+                        <option value="grade">등급</option>
+                    </select>
+                </div>
+            </div>
         </div>
     );
 }
