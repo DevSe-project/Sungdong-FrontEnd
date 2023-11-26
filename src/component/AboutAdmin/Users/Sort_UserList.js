a15a05bbb28411e1864a58931740b40b340e3d6b
jsum01 <djadjawltjr@gmail.com>
2023-11-26 23:00:54 +0900

    Update 고객관리-정렬: 퍼블리싱(데모) 및 기능

--- src/component/AboutAdmin/Users/Sort_UserList.js
+++ src/component/AboutAdmin/Users/Sort_UserList.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import styles from './Sort_UserList.module.css';
 
-export default function Sort_UserList() {
+const Sort_UserList = (props) => {
+
+    const handleSort = () => {
+        props.onSort();
+    };
+
+    // 추가 예정 기능 - 정렬이 2개 이상 선택되면 경고문구 표시
+
     return (
-        <div>
-            {/* Sorting options */}
-            <button className={styles.button}>등급 정렬</button>
-            <button className={styles.button}>기업명 정렬</button>
+        <div className={styles.sortContainer}>
+
+            <h3>정렬 우선순위 설정</h3>
+
+            <div className={styles.sortItems}>
+                {[1, 2, 3].map((priority) => (
+                    <label key={priority}>
+                        {priority}
+                        <select
+                            value={props.sortBy[priority - 1]}
+                            onChange={(e) => onSort(e.target.value, priority - 1)}>
+                            <option value="usertype">구분</option>
+                            <option value="bizname">업체명</option>
+                            <option value="grade">등급</option>
+                        </select>
+                    </label>
+                ))}
+
+                {/* 다음 미팅: 고객 리스트 중 하나를 클릭하면 고객 상세 정보를 볼 수 있다. 
+                        리스트에서 바로 보이도록 하고 싶은 데이터가 무엇이 있는지 취합하고, 
+                        이 데이터를 option으로 추가 예정 */}
+
+            </div>
+            <div>
+                <button className={styles.button} onClick={handleSort}>정렬하기</button>
+            </div>
         </div>
     );
-}
+};
+
+export default Sort_UserList;
