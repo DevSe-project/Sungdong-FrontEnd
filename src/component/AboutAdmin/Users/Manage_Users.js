b1067f180d695b3ab34cfa7ea336dc20e51112d0
jsum01 <djadjawltjr@gmail.com>
2023-11-26 22:21:13 +0900

    Update 고객관리p: 컴포넌트 생성~db데이터 받아서 리스트업 및 외부 컴포넌트 배치

--- src/component/AboutAdmin/Users/Manage_Users.js
+++ src/component/AboutAdmin/Users/Manage_Users.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import { db } from '../../../firebase';
+// Manage_Users.js
+
+import React, { useState, useMemo } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { AdminHeader } from '../AdminHeader';
 import { AdminMenuData } from '../AdminMenuData';
 import FilterSearch_User from './FilterSearch_User';
-import styles from './Manage_Users.module.css';
 import Sort_UserList from './Sort_UserList';
-import { useQuery } from '@tanstack/react-query';
-
+import styles from './Manage_Users.module.css';
 
 export default function Manage_Users() {
-    
-    const { data, isError, error, isLoading } = useQuery({
+    const { data, isError, isLoading } = useQuery({
         queryKey: ['users']
     });
 
+    const [sortedData, setSortedData] = useState(data);
+    const [sortBy, setSortBy] = useState([]);
+
+    const handleSort = () => {
+        // 정렬 로직 구현
+        // sortBy 배열을 기반으로 데이터를 정렬하고 setSortedData를 통해 업데이트
+        // 예: 정렬 로직 구현 후 setSortedData(sortedData)
+    };
+
+    useMemo(() => {
+        // data나 sortBy가 변경될 때마다 정렬
+        // handleSort();
+    }, [data, sortBy]);
+
+    const handleDelete = (id) => {
+        // 회원 삭제 로직 구현
+        // 예: 삭제 로직 구현 후 setSortedData(filteredData)
+    };
+
     if (isLoading) {
-        return <div>Loading...</div>;
+        return <div>로딩 중...</div>;
     }
 
     if (isError) {
-        return <div>Error fetching data</div>;
+        return <div>오류가 발생했습니다.</div>;
     }
 
     return (
@@ -28,30 +46,33 @@ export default function Manage_Users() {
             <div className={styles.body}>
                 <AdminMenuData />
                 <div className={styles.mainContainer}>
-                    <div className={styles.filtSort_container}>
+                    <div className={styles.filtSortContainer}>
                         <FilterSearch_User />
-                        <Sort_UserList />
+                        <Sort_UserList sortBy={sortBy} onSort={handleSort} />
                     </div>
-                    <table>
+                    <table className={styles.userTable}>
                         <thead>
                             <tr>
                                 <th>구분</th>
+                                <th>업체명</th>
                                 <th>등급</th>
-                                <th>이름</th>
-                                <th>비밀번호</th>
                                 <th>CMS 여부</th>
-                                <th>회원 탈퇴</th>
+                                <th>작업</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {data.map((user, index) => (
+                            {sortedData.map((user, index) => (
                                 <tr key={index}>
                                     <td>{user.usertype}</td>
+                                    <td>{user.bizname}</td>
                                     <td>{user.grade}</td>
-                                    <td>{user.name}</td>
-                                    <td>{user.password}</td>
-                                    <td>{user.CMS}</td>
-                                    <td>회원삭제</td>
+                                    <td>{user.CMS ? "동의" : "비동의"}</td>
+                                    <td>
+                                        <button className={styles.button}
+                                            onClick={() => handleDelete(user.id)}>
+                                            회원 삭제
+                                        </button>
+                                    </td>
                                 </tr>
                             ))}
                         </tbody>
