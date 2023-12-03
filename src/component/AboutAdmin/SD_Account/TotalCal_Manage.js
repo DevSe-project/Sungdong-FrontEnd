01301f501fac579f9c39c165621b74c8604e1ccf
jsum01 <djadjawltjr@gmail.com>
2023-12-03 22:21:27 +0900

    Update 누적정산p: 30개 임시 데이터&&종류별 정산 기능 주석추가

--- src/component/AboutAdmin/SD_Account/TotalCal_Manage.js
+++ src/component/AboutAdmin/SD_Account/TotalCal_Manage.js
@@ -1,25 +1,64 @@
-// TotalCal_Manage.js
-
 import React from 'react';
 import styles from './TotalCal_Manage.module.css';
 import { AdminHeader } from '../AdminHeader';
 import { AdminMenuData } from '../AdminMenuData';
 
 export default function TotalCal_Manage() {
-    // 가상의 데이터 배열 (실제 데이터는 서버에서 가져와야 함)
+    // 임시 총 주문 데이터 30개
     const transactionData = [
         { id: 1, name: '회원1', date: '2023-01-01', description: '상품 구매', amount: 100000, status: '완료' },
-        // 다른 트랜잭션들 추가
+        { id: 2, name: '회원2', date: '2023-01-02', description: '서비스 이용', amount: 75000, status: '처리 중' },
+        { id: 3, name: '회원3', date: '2023-01-03', description: '할인 적용', amount: 120000, status: '완료' },
+        { id: 4, name: '회원4', date: '2023-01-04', description: '포인트 사용', amount: 90000, status: '취소' },
+        { id: 5, name: '회원5', date: '2023-01-05', description: '상품 구매', amount: 80000, status: '완료' },
+        { id: 6, name: '회원6', date: '2023-01-06', description: '서비스 이용', amount: 60000, status: '완료' },
+        { id: 7, name: '회원7', date: '2023-01-07', description: '할인 적용', amount: 130000, status: '처리 중' },
+        { id: 8, name: '회원8', date: '2023-01-08', description: '포인트 사용', amount: 70000, status: '완료' },
+        { id: 9, name: '회원9', date: '2023-01-09', description: '기타', amount: 110000, status: '처리 중' },
+        { id: 10, name: '회원10', date: '2023-01-10', description: '상품 구매', amount: 95000, status: '취소' },
+        { id: 11, name: '회원11', date: '2023-01-11', description: '서비스 이용', amount: 70000, status: '완료' },
+        { id: 12, name: '회원12', date: '2023-01-12', description: '할인 적용', amount: 120000, status: '처리 중' },
+        { id: 13, name: '회원13', date: '2023-01-13', description: '기타', amount: 85000, status: '취소' },
+        { id: 14, name: '회원14', date: '2023-01-14', description: '상품 구매', amount: 90000, status: '완료' },
+        { id: 15, name: '회원15', date: '2023-01-15', description: '포인트 사용', amount: 110000, status: '처리 중' },
+        { id: 16, name: '회원16', date: '2023-01-16', description: '할인 적용', amount: 130000, status: '완료' },
+        { id: 17, name: '회원17', date: '2023-01-17', description: '상품 구매', amount: 100000, status: '처리 중' },
+        { id: 18, name: '회원18', date: '2023-01-18', description: '기타', amount: 95000, status: '완료' },
+        { id: 19, name: '회원19', date: '2023-01-19', description: '포인트 사용', amount: 80000, status: '완료' },
+        { id: 20, name: '회원20', date: '2023-01-20', description: '할인 적용', amount: 120000, status: '취소' },
+        { id: 21, name: '회원21', date: '2023-01-21', description: '상품 구매', amount: 100000, status: '완료' },
+        { id: 22, name: '회원22', date: '2023-01-22', description: '서비스 이용', amount: 75000, status: '처리 중' },
+        { id: 23, name: '회원23', date: '2023-01-23', description: '할인 적용', amount: 120000, status: '완료' },
+        { id: 24, name: '회원24', date: '2023-01-24', description: '포인트 사용', amount: 90000, status: '취소' },
+        { id: 25, name: '회원25', date: '2023-01-25', description: '상품 구매', amount: 80000, status: '완료' },
+        { id: 26, name: '회원26', date: '2023-01-26', description: '서비스 이용', amount: 60000, status: '완료' },
+        { id: 27, name: '회원27', date: '2023-01-27', description: '할인 적용', amount: 130000, status: '처리 중' },
+        { id: 28, name: '회원28', date: '2023-01-28', description: '포인트 사용', amount: 70000, status: '완료' },
+        { id: 29, name: '회원29', date: '2023-01-29', description: '기타', amount: 110000, status: '처리 중' },
+        { id: 30, name: '회원30', date: '2023-01-30', description: '상품 구매', amount: 95000, status: '취소' },
     ];
 
+    // -----------------삽입 예상 기능----------------- //
+    // 총 정산 완료 기대 금액
+    const totalAmount = transactionData.reduce((acc, transaction) => acc + transaction.amount, 0);
+    // 정산 완료 금액
+
+    // 정산 대기 금액
+
+    // 정상 예정 금액
+
+    // 
+    // -------------------------------------------- //
+
     return (
-        <div className={styles.body}>
+        <div>
             <AdminHeader />
-            <div className={styles.container}>
+            <div className={styles.flex_container}>
                 <AdminMenuData />
-                <div className={styles.mainContainer}>
+                <div className={styles.main_container}>
                     <header className={styles.header}>
-                        <h1>회원 정산 페이지</h1>
+                        {/* 누적 정산 금액 */}
+                        <h3>누적 정산 금액: <span className={styles.totalAccount}>{totalAmount}</span>원</h3>
                     </header>
                     <main className={styles.main}>
                         {/* 정산 내용을 표시하는 테이블 */}
@@ -48,7 +87,9 @@ export default function TotalCal_Manage() {
                             </tbody>
                         </table>
                         {/* 정산 버튼 */}
-                        <button className={styles.btn}>정산하기</button>
+                        <div className={styles.btn_container}>
+                            <button className={styles.btn}>정산하기</button>
+                        </div>
                     </main>
                 </div>
             </div>
