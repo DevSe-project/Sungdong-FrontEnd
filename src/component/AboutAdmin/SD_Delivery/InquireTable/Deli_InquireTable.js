1a73cff31dbcbc6fdc8b0e607a36ee1dda0102bb
jsum01 <djadjawltjr@gmail.com>
2024-01-05 11:03:07 +0900

    Update Deli_InquireTable : Complete

---
+++ src/component/AboutAdmin/SD_Delivery/InquireTable/Deli_InquireTable.js
@@ -0,0 +1,142 @@
+import { useState, useEffect } from 'react';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import styles from './Deli_InquireTable.module.css';
+export default function Deli_InquireTable() {
+
+    const queryClient = useQueryClient();
+
+    // 게시물 데이터와 페이지 번호 상태 관리    
+    const [currentPage, setCurrentPage] = useState(1);
+    const [matchedData, setMatchedData] = useState([]);
+
+
+    // Fetch
+    const { isLoading: deliveryLoading, isError: deliveryError, data: delivery } = useQuery({ queryKey: ['delivery'] });
+    const { isLoading: orderedLoading, isError: orderedError, data: ordered } = useQuery({ queryKey: ['ordered'] });
+    const { isLoading: productLoading, isError: productError, data: product } = useQuery({ queryKey: ['data'] });
+
+
+
+    // 업데이트 함수 호출
+    useEffect(() => {
+        updateMatchedData();
+    }, [ordered, delivery, product]);
+
+
+
+    // 현재 페이지에 해당하는 게시물 목록 가져오기
+    const getCurrentPagePosts = () => {
+        const startIndex = (currentPage - 1) * 5;
+        return matchedData.slice(startIndex, startIndex + 5);
+    };
+
+
+    // 상태 업데이트를 위한 함수
+    const updateMatchedData = () => {
+        if (!ordered || !delivery || !product) {
+            return;
+        }
+
+        // orderID를 기준으로 ordered와 delivery를 매칭
+        const matchedDelivery = ordered.map(orderItem => {
+            const deliveryItem = delivery.find(deliveryItem => deliveryItem.orderID === orderItem.orderID);
+            return { ...orderItem, ...deliveryItem };
+        });
+
+        // productId를 기준으로 matchedDelivery와 product를 매칭
+        const finalMatchedData = matchedDelivery.map(matchedItem => {
+            const productItem = product.find(productItem => productItem.productId === matchedItem.productId);
+            return { ...matchedItem, ...productItem };
+        });
+
+        setMatchedData(finalMatchedData);
+    };
+
+
+
+    // 데이터 로딩 중 또는 에러 발생 시 처리
+    if (deliveryLoading || orderedLoading || productLoading) {
+        return <p>Loading...</p>;
+    }
+    if (deliveryError || orderedError || productError) {
+        return <p>Error fetching data</p>;
+    }
+
+
+    return (
+        <div className={styles.body}>
+            {/* header */}
+            <div className={styles.header}>
+                {/* Post */}
+                <div className={styles.header_txt}>
+                    목록
+                </div>
+                {/* Number of Denote */}
+                <select className={styles.denoteNumber_select}>
+                    <option>10</option>
+                    <option>30</option>
+                    <option>50</option>
+                </select>
+            </div>
+            {/* main */}
+            <table>
+                <thead
+                    style={{ backgroundColor: 'white', color: 'black', boxShadow: '0 1px 2px rgba(0, 0, 0, 0.2)' }}
+                >
+                    <tr>
+                        <th>주문번호</th>
+                        <th>처리상태</th>
+                        <th>주문일자</th>
+                        <th>상품코드</th>
+                        <th>이미지</th>
+                        <th>상품명</th>
+                        <th>옵션명</th>
+                        <th>표준가</th>
+                        <th style={{ fontWeight: '650' }}>공급가</th>
+                        <th>더보기</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {getCurrentPagePosts().map((item, index) => (
+                        <tr key={index}>
+                            <td>{item.orderID}</td> {/* 주문번호 */}
+                            <td>{item.deliveryType}</td> {/* 처리상태 */}
+                            <td>{item.order_Date}</td> {/* 주문일자 */}
+                            <td>{item.ProductId}</td> {/* 상품코드 */}
+                            <td>{item.image.mini}</td> {/* 이미지 */}
+                            <td>{item.title}</td> {/* 상품명 */}
+                            <td>{item.option ? item.option : "-"}</td> {/* 옵션명 */}
+                            <td>{item.price}</td> {/* 표준가 */}
+                            <td>{item.discount == 0 ? item.price : item.price - (item.price * item.discount/100)}</td> {/* 공급가 */}
+                            <td>더보기</td> {/* 더보기 */}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            <div className={styles.pageMoveHandler}>
+                {/* 이전 페이지 */}
+                <button className={styles.moveButton} onClick={() => {
+                    if (currentPage !== 1) {
+                        setCurrentPage(currentPage - 1);
+                    } else {
+                        alert("해당 페이지가 가장 첫 페이지 입니다.");
+                    }
+                }}>
+                    <i className="far fa-angle-left" />
+                </button>
+                <div className={styles.currentPage}> {currentPage} </div>
+                {/* 다음 페이지 */}
+                <button className={styles.moveButton} onClick={() => {
+                    if (matchedData.length > currentPage * 5) {
+                        setCurrentPage(currentPage + 1);
+                    } else {
+                        alert("다음 페이지가 없습니다.");
+                    }
+                }}>
+                    <i className="far fa-angle-right" />
+                </button>
+            </div>
+        </div>
+    );
+}
