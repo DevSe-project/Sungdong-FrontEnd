c8efcfcfabe415b6a0b2cbf6046285f1bf3caf5c
jsum01 <djadjawltjr@gmail.com>
2024-01-05 11:02:31 +0900

    Update path - Deli_Filter, DeliveryManagement

---
+++ src/component/AboutAdmin/SD_Delivery/Filter/Deli_Filter.js
@@ -0,0 +1,77 @@
+import styles from './Deli_Filter.module.css';
+
+export default function Deli_Filter() {
+
+    function deliStateFtilter() {
+        return (
+            <div className={styles.deliState_container}>
+                {/* 전체 */}
+                <input className={styles.state_checkbox} id='deliState_all' type='checkbox' /> 
+                <label className={styles.state_label} htmlFor='deliState_all'>전체</label>
+                {/* 배송 준비 */}
+                <input className={styles.state_checkbox} id='deliState_ready' type='checkbox' /> 
+                <label className={styles.state_label} htmlFor='deliState_ready'>배송 준비</label>
+                {/* 배송 진행중 */}
+                <input className={styles.state_checkbox} id='deliState_ing' type='checkbox' /> 
+                <label className={styles.state_label} htmlFor='deliState_ing'>배송 진행</label>
+                {/* 배송 완료 */}
+                <input className={styles.state_checkbox} id='deliState_ed' type='checkbox' /> 
+                <label className={styles.state_label} htmlFor='deliState_ed'>배송 완료</label>
+                {/* 배송 지연 */}
+                <input className={styles.state_checkbox} id='deliState_delay' type='checkbox' />
+                <label className={styles.state_label} htmlFor='deliState_delay'>배송 지연</label>
+            </div>
+        )
+    }
+
+    function dateFilter() {
+        return (
+            <div className={styles.dateFilter_container}>
+                <input className={styles.date} type='date' />
+                ~
+                <input className={styles.date} type='date' />
+                <div className={styles.dateButton} onClick={ () => {} }> 오늘 </div>
+                <div className={styles.dateButton} onClick={ () => {} }> 1 주일 </div>
+                <div className={styles.dateButton} onClick={ () => {} }> 1 개월 </div>
+                <div className={styles.dateButton} onClick={ () => {} }> 3 개월 </div>
+                <div className={styles.dateButton} onClick={ () => {} }> 6 개월 </div>
+            </div>
+        )
+    }
+
+    const filterList = [
+        { label: '배송상태', content: deliStateFtilter() },
+        { label: '기간', content: dateFilter() },
+    ]
+
+    return (
+        <div style={{ width: '100%' }}>
+            <form className={styles.main}>
+                <div style={{ width: '90%', textAlign: 'left', padding: '1.5em', borderBottom: '1px solid lightgray' }}>
+                    <h4 style={{ fontSize: '1.2em', fontWeight: '650' }}>필터</h4>
+                </div>
+                {/* Map */}
+                {filterList.map((item, index) => (
+                    // List Container
+                    <div key={index} className={styles.container}>
+
+                        {/* Label */}
+                        <div className={styles.label}>
+                            {item.label}
+                        </div>
+
+                        {/* Content */}
+                        <div className={styles.content}>
+                            {item.content}
+                        </div>
+
+                    </div>
+                ))}
+                <div style={{ display: 'flex', gap: '0.5em' }}>
+                    <input className={styles.button} type='submit' value='검색' />
+                    <input className={styles.button} type='reset' onClick={() => {}} />
+                </div>
+            </form>
+        </div>
+    )
+}
---
+++ src/component/AboutAdmin/SD_Delivery/State_M/DeliveryManagement.js
@@ -0,0 +1,42 @@
+
+import { AdminHeader } from '../../Layout/Header/AdminHeader';
+import { AdminMenuData } from '../../Layout/SideBar/AdminMenuData';
+import Deli_Filter from '../Filter/Deli_Filter';
+import Deli_InquireTable from '../InquireTable/Deli_InquireTable';
+import styles from './DeliveryManagement.module.css';
+
+
+export default function DeliveryManagement() {
+
+
+
+    return (
+        // Full Screen
+        <div>
+
+            <AdminHeader />
+
+            {/* Body */}
+            <div className={styles.body}>
+
+                <AdminMenuData />
+
+                {/* Main */}
+                <div className={styles.main}>
+
+                    {/* Post */}
+                    <div className={styles.main_post}> 배송 상태 관리  </div>
+
+                    {/* Filter Container */}
+                    <Deli_Filter />
+
+                    {/* Delivery InquireTable Header */}
+                    <Deli_InquireTable />
+                    
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
