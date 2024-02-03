0a758bf715e14c5adb599adcc635d8a348131bee
taehun2123 <pyoneng_@naver.com>
2024-02-03 11:42:19 +0900

    Update: 페이지네이션 디자인 변경 및 10페이지 단위로 페이지 수 출력하기

--- src/customFn/Pagenation.js
+++ src/customFn/Pagenation.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+    const maxDisplayedPages = 10; // 최대 표시할 페이지 수
+
+    const startPage = Math.floor((currentPage - 1) / maxDisplayedPages) * maxDisplayedPages + 1;
+    const endPage = Math.min(startPage + maxDisplayedPages - 1, totalPages);
+
     return (
     <div className="buttonContainer">
     {/* 페이지네이션을 화면에 표시 */}
-    {Array.from({ length: totalPages }, (_, index) => index + 1).map(pageNumber => (
+    {Array.from({ length: endPage - startPage + 1 }, (_, index) => index + 1).map(pageNumber => (
         <span
         key={pageNumber}
         onClick={() => onPageChange(pageNumber)}
@@ -13,6 +18,15 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
         {pageNumber}
         </span>
     ))}
+          {/* 10페이지를 넘어가면 다음 화살표 버튼 */}
+        {endPage < totalPages && (
+        <span
+        onClick={() => onPageChange(currentPage + 1)}
+        className="nextPageButton"
+        >
+        <i className="far fa-angle-right"/>
+        </span>
+        )}
     </div>
     );
 };
