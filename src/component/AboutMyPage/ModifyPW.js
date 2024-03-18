4b6fdd8bd5fc458035ecbeb7f323281668bf5e3e
jsum01 <djadjawltjr@gmail.com>
2024-03-19 00:10:04 +0900

    Updating... ModifyPW: 비밀번호 변경 API  구현 중...

--- src/component/AboutMyPage/ModifyPW.js
+++ src/component/AboutMyPage/ModifyPW.js
@@ -1,181 +1,143 @@
 import { React, useEffect, useState } from 'react';
 import styles from './ModifyPW.module.css';
 import { useDataActions, useModal, useModalActions, useUserData } from '../../store/DataStore';
+import { useCheck } from '../../customFn/useChecking';
 
 export default function ModifyPW(props) {
+
+  const { closeModal } = useModalActions(); // Modal
+  const { handleItemChecking, handleInputLength } = useCheck();
+
+
+  // 현재비번, 재설정비번, 재설정비번확인 Input
+  const [inputForModify, setInputForModify] = useState({
+    now_password: null,
+    re_password: null,
+    confirm_re_password: null
+  })
+
+
+  // enter키를 누르면 'PW찾기'
+  /**
+   * Input 내부에서 'Enter'키를 누를 시, goModify()가 실행되도록 함
+   * @param {*} event 
+   * @returns goModify()
+   */
+  function handleEnter_pwFind(event) {
+    if (event.key === 'Enter') {
+      goModify();
+    }
+  }
+
+  /** 
+   * input 값에 따른 스타일 변경에 필요한 함수 
+   * */
+  function handleStyle() {
+    return handleInputLength(inputForModify.confirm_re_password) && handleItemChecking(inputForModify.re_password, inputForModify.confirm_re_password) ? 'rgb(240, 255, 230)' : 'lightgray'
+  }
+
+  // 비밀번호 변경 실행함수
+  function goModify() {
     
-    const { closeModal } = useModalActions();
-    const userData = useUserData();
-    const {setUserData} = useDataActions();
+  }
+
+  // esc키를 누르면 모달창 닫기.
+  useEffect(() => {
+    const exit_esc = (event) => {
+      if (event.key === 'Escape') {
+        closeModal(); // "Esc" 키 누를 때 모달 닫기 함수 호출
+      }
+    };
+
+    window.addEventListener('keydown', exit_esc);
+
+    return () => {
+      window.removeEventListener('keydown', exit_esc);
+    };
+  }, [closeModal]);
 
 
-    // 현재비번, 재설정비번, 재설정비번확인 Input
-    const [inputForModify, setInputForModify] = useState({
-        now_password: null,
-        re_password: null,
-        confirm_re_password: null
-    })
-
-    // 재설정 비번 일치확인
-    const isPwEqual = inputForModify.re_password === inputForModify.confirm_re_password;
-    // 입력유무 확인
-    const isInputNull = inputForModify.re_password !== null && inputForModify.confirm_re_password != null;
-
-    // 비밀번호 수정 함수. => 백앤드로 편입할 함수
-    function goModify() {
-        const confirmPwFind = userData.find(userData => userData.password === inputForModify.now_password);
-
-        if (confirmPwFind && confirmPwFind.password === inputForModify.now_password) {
-            if (isInputNull) { // 입력유무 감지
-                if (isPwEqual) { // 비밀번호 일치 확인
-                    if (inputForModify.re_password.length >= 8 && inputForModify.confirm_re_password.length >= 8) {// 입력 값 8글자 이상
-
-                        // 비밀번호 변경 로직 추가
-                        const updatedUserData = userData.map(userData => {
-                            if (userData.password === inputForModify.now_password) {
-                                return { ...userData, password: inputForModify.re_password };
-                            }
-                            return userData;
-                        });
-                        setUserData(updatedUserData); // 사용자 데이터 업데이트
-
-                        alert("변경 완료");
-                    } else {
-                        alert("비밀번호 8글자 이상 입력하십시오.");
-                    }
-                } else {
-                    alert("※비밀번호 불일치※")
-                }
-            } else {
-                alert("입력하지 않은 란 존재");
-            }
-        } else {
-            alert("정확하지 않은 비밀번호");
-        }
-    }
-
-
-
-
-    // enter키를 누르면 'PW찾기'
-    function handleEnter_pwFind(event) {
-        if (event.key === 'Enter') {
-            goModify();
-        }
-    }
-
-    // esc키를 누르면 모달창 닫기.
-    useEffect(() => {
-        const exit_esc = (event) => {
-            if (event.key === 'Escape') {
-                closeModal(); // "Esc" 키 누를 때 모달 닫기 함수 호출
-            }
-        };
-
-        window.addEventListener('keydown', exit_esc);
-
-        return () => {
-            window.removeEventListener('keydown', exit_esc);
-        };
-    }, [closeModal]);
-
-
-    return (
-        <div className={styles.modalOverlay}>
-            <div className={styles.modalContainer}>
-                <div className={styles.exitButton}>
-                    <span onClick={closeModal}>
-                        <i class="fas fa-times"></i>
-                    </span>
-                </div>
-                <div className={styles.modalContent}>
-                    <div className={styles.titleBox}>
-                        <div className={`${styles.title}`}>
-                            비밀번호 수정
-                        </div>
-                    </div>
-                    {/* 비밀번호 수정 모달 내부 컴포넌트 */}
-                    <ModifyInput
-                        inputForModify={inputForModify}
-                        setInputForModify={setInputForModify}
-                        goModify={goModify}
-                        handleEnter_pwFind={handleEnter_pwFind}
-                    />
-                </div>
-            </div>
+  return (
+    <div className={styles.modalOverlay}>
+      <div className={styles.modalContainer}>
+        <div className={styles.exitButton}>
+          <span onClick={closeModal}>
+            <i class="fas fa-times"></i>
+          </span>
         </div>
-    );
-}
+        <div className={styles.modalContent}>
+          {/* Title */}
+          <div className={styles.titleBox}>
+            <div className={`${styles.title}`}>
+              비밀번호 수정
+            </div>
+          </div>
 
-// ProveUser 모달 컴포넌트
-function ModifyInput(props) {
-
-    return (
-        <div className={styles.contents_container}>
+          <div className={styles.contents_container}>
             <div className={styles.pw_layout}>
-                <div className={styles.inputContainer}>
-                    <div className={styles.label}>현재 <br />비밀번호</div>
-                    <div className={styles.input}>
-                        <input
-                            className={styles.input}
-                            type='password'
-                            placeholder='현재 비밀번호를 입력'
-                            value={props.inputForModify.now_password}
-                            onChange={(e) => {
-                                const inputPassword = {
-                                    ...props.inputForModify,
-                                    now_password: e.target.value
-                                }
-                                props.setInputForModify(inputPassword);
-                            }}
-                        />
-                    </div>
-                </div>
-                <div className={styles.inputContainer}>
-                    <div className={styles.label}>재설정 <br />비밀번호</div>
-                    <div className={styles.input}>
-                        <input
-                            className={styles.input}
-                            type='password'
-                            placeholder='재설정할 비밀번호 입력'
-                            value={props.inputForModify.re_password}
-                            onChange={(e) => {
-                                const inputCeoName = {
-                                    ...props.inputForModify,
-                                    re_password: e.target.value
-                                };
-                                props.setInputForModify(inputCeoName);
-                            }}
-                            onKeyDown={props.handleEnter_idFind}
-                        />
-                    </div>
-                </div>
-                <div className={styles.inputContainer}>
-                    <div className={styles.label}>재설정 <br />비밀번호 <br />확인</div>
-                    <div className={styles.input}>
-                        <input
-                            className={styles.input}
-                            type='password'
-                            placeholder='재설정할 비밀번호 확인'
-                            value={props.inputForModify.confirm_re_password}
-                            onChange={(e) => {
-                                const inputbiz_num = {
-                                    ...props.inputForModify,
-                                    confirm_re_password: e.target.value
-                                };
-                                props.setInputForModify(inputbiz_num);
-                            }}
-                            onKeyDown={props.handleEnter_pwFind}
-                        />
-                    </div>
-                </div>
+              <div className={styles.inputContainer}>
+                <div className={styles.label}>현재 <br />비밀번호</div>
+                <input
+                  className={styles.input}
+                  type='password'
+                  placeholder='현재 비밀번호를 입력'
+                  value={inputForModify.now_password}
+                  onChange={(e) => {
+                    const inputPassword = {
+                      ...inputForModify,
+                      now_password: e.target.value
+                    }
+                    setInputForModify(inputPassword);
+                  }}
+                  onKeyDown={handleEnter_pwFind}
+                />
+              </div>
+              <div className={styles.inputContainer}>
+                <div className={styles.label}>재설정 <br />비밀번호</div>
+                <input
+                  className={styles.input}
+                  type='password'
+                  placeholder='재설정할 비밀번호 입력'
+                  value={inputForModify.re_password}
+                  onChange={(e) => {
+                    const inputCeoName = {
+                      ...inputForModify,
+                      re_password: e.target.value
+                    };
+                    setInputForModify(inputCeoName);
+                  }}
+                  onKeyDown={handleEnter_pwFind}
+                  style={{ backgroundColor: handleStyle() }}
+                />
+              </div>
+              <div className={styles.inputContainer}>
+                <div className={styles.label}>재설정 <br />비밀번호 <br />확인</div>
+                <input
+                  className={styles.input}
+                  type='password'
+                  placeholder='재설정할 비밀번호 확인'
+                  value={inputForModify.confirm_re_password}
+                  onChange={(e) => {
+                    const inputbiz_num = {
+                      ...inputForModify,
+                      confirm_re_password: e.target.value
+                    };
+                    setInputForModify(inputbiz_num);
+                  }}
+                  onKeyDown={handleEnter_pwFind}
+                  style={{ backgroundColor: handleStyle() }}
+                />
+              </div>
             </div>
             <div
-                className={styles.goModify}
-                onClick={props.goModify}>
-                비밀번호<br />
-                변경하기
+              className={styles.goModify}
+              onClick={() => { }}>
+              비밀번호<br />
+              변경하기
             </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
