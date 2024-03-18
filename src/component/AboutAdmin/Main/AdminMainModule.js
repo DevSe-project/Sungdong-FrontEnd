e22cd68c7c12d2eafbe64813c82e1daeb41abc00
taehun2123 <pyoneng_@naver.com>
2024-03-18 15:27:18 +0900

    Update: 주문 관련 건수 API, 반품,교환,취소 관련 건수 API 요청 및 표시 완료

--- src/component/AboutAdmin/Main/AdminMainModule.js
+++ src/component/AboutAdmin/Main/AdminMainModule.js
@@ -1,5 +1,7 @@
 import { AdminMainListModule } from './AdminMainListModule'
 import styles from './AdminMain.module.css'
+import { useQuery } from '@tanstack/react-query';
+import { useFetch } from '../../../customFn/useFetch';
 export function AdminMainModule() {
   function depositIcon() {
     return <i style={{ fontSize: '2em', color: 'green' }} className="fas fa-won-sign"></i>
@@ -7,6 +9,70 @@ export function AdminMainModule() {
   function cancelIcon() {
     return <i style={{ fontSize: '2em', color: 'lightBlue' }} className="fas fa-undo-alt"></i>
   }
+
+  //Fetch Custom Hooks
+  const { fetchServer, fetchGetServer } = useFetch();
+
+    /**
+   * @불러오기
+   * 페이지를 불러올 때 선언되는 GET FETCH
+   * - setCurrentPage - 현재 페이지 개수 지정
+   * - setTotalPages - 전체 페이지 개수 지정
+   * @returns 전체 이벤트 조회
+   * - event_id - 이벤트 고유번호
+   * - event_image : 이벤트 이미지
+   * - event_content : 이벤트 정보(설명)
+   * - event_title : 이벤트 타이틀명
+   * - event_startDate : 이벤트 시작일
+   * - event_endDate : 이벤트 종료일
+   */
+    const fetchData = async () => {
+      const data = await fetchGetServer("/rae/admin/module", 1);
+      return data;
+    };
+  
+    const {
+      isLoading,
+      isError,
+      error,
+      data: moduleData,
+    } = useQuery({
+      queryKey: ["moduleRae"],
+      queryFn: () => fetchData(),
+    });
+
+  /**
+   * @불러오기
+   * 페이지를 불러올 때 선언되는 GET FETCH
+   * - setCurrentPage - 현재 페이지 개수 지정
+   * - setTotalPages - 전체 페이지 개수 지정
+   * @returns 전체 이벤트 조회
+   * - event_id - 이벤트 고유번호
+   * - event_image : 이벤트 이미지
+   * - event_content : 이벤트 정보(설명)
+   * - event_title : 이벤트 타이틀명
+   * - event_startDate : 이벤트 시작일
+   * - event_endDate : 이벤트 종료일
+   */
+      const fetchOrderData = async () => {
+        const data = await fetchGetServer("/order/admin/module", 1);
+        return data;
+      };
+    
+      const {
+        data: orderData,
+      } = useQuery({
+        queryKey: ["moduleOrder"],
+        queryFn: () => fetchOrderData(),
+      });
+
+    if (isLoading) {
+      return <p>Loading..</p>;
+    }
+    if (isError) {
+      return <p>에러 : {error.message}</p>;
+    }
+
   return (
     <div className={styles.main}>
 
@@ -17,12 +83,12 @@ export function AdminMainModule() {
         <article className={styles.product}>
           {/* 입금 */}
           <div className={styles.productSeparate}>
-            <AdminMainListModule icon={depositIcon()} firstName={"입금대기"} secondName={"신규주문"} thirdName={"오늘출발"} />
+            <AdminMainListModule icon={depositIcon()} firstName={"입금대기"} secondName={"신규주문"} thirdName={"오늘출발"} first={orderData[0]?.noPay} second={orderData[0]?.pay} third={orderData[0]?.todayDelivery} />
           </div>
 
           {/* 취소 */}
           <div className={styles.productSeparate}>
-            <AdminMainListModule icon={cancelIcon()} firstName={"취소요청"} secondName={"반품요청"} thirdName={"교환요청"} />
+            <AdminMainListModule icon={cancelIcon()} firstName={"반품요청"} secondName={"교환요청"} thirdName={"취소요청"} first={moduleData[0]?.refund} second={moduleData[0]?.exchange} third={moduleData[0]?.cancel} />
           </div>
 
           {/* 배송 */}
