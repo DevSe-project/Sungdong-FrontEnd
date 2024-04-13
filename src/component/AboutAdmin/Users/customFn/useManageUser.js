782fe6f9ca13f5043c26120f9ecbf9c07858582c
jsum01 <djadjawltjr@gmail.com>
2024-04-13 13:23:09 +0900

    Refactor CRUD API useManageUser커스텀 함수에서 분리
    업데이트, 삭제 기능 각 컴포넌트로 배치
    
    위치: userManageUser -> Admin[Done&Hold]User

--- src/component/AboutAdmin/Users/customFn/useManageUser.js
+++ src/component/AboutAdmin/Users/customFn/useManageUser.js
@@ -31,99 +31,105 @@ export default function useManagerUser() {
     }
   }
 
-  // 고객 단일 수정
-  const handleEdit = async (userData) => { // 
-    try {
-      const confirmMessage = '수정사항을 반영하시겠습니까?';
-      const confirmed = window.confirm(confirmMessage);
-      if (!confirmed) return;
+  
 
-      await editMutation(userData);
-      window.location.reload();
-    } catch (error) {
-      console.error('유저 수정 실패:', error);
-      alert('유저 수정에 실패했습니다.');
-    }
-  };
-  // 전체 수정 함수(정상작동 확인))
-  const handleBulkEdit = async () => {
-    try {
-      const confirmMessage = '수정사항을 반영하시겠습니까?';
-      const confirmed = window.confirm(confirmMessage);
-      if (!confirmed) return;
-      const bulkUserData = []; // 수정된 데이터를 담을 배열을 초기화
-      // 선택된 모든 사용자의 수정된 데이터를 추출하여 배열에 추가합니다.
-      checkedItems.forEach((checkedID) => {
-        const editedUserData = matchedData.find((user) => user.users_id === checkedID);
-        bulkUserData.push(editedUserData);
-      });
-      await editMutation(bulkUserData); // 수정된 사용자 데이터 서버로 전송
-    } catch (error) {
-      console.error('전체 수정 실패:', error);
-      alert('전체 수정에 실패했습니다.');
-    }
-  };
-  const handleEditSuccess = (data) => { // 수정 성공
-    console.log('수정 성공:', data);
-    alert('수정이 완료되었습니다.');
-    queryClient.invalidateQueries('holdusers');
-    setEditIndex(null);
-  };
-  const handleEditError = (error) => { // 수정 실패
-    console.error('수정 실패:', error);
-    alert('수정에 실패했습니다.');
-  };
-  const fetchEditUser = async (userData) => { // Fetching
-    try {
-      const response = await axios.post('/auth/update', userData);
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  };
-  const { mutate: editMutation } = useMutation({ // Mutation
-    mutationFn: fetchEditUser,
-    onSuccess: handleEditSuccess,
-    onError: handleEditError,
-  });
+  // // ------------------------고객정보 업데이트 기능 시작------------------------ //
+  // // 고객 단일 수정
+  // const handleEdit = async (userData) => { // 
+  //   try {
+  //     const confirmMessage = '수정사항을 반영하시겠습니까?';
+  //     const confirmed = window.confirm(confirmMessage);
+  //     if (!confirmed) return;
+
+  //     await editMutation(userData);
+  //     window.location.reload();
+  //   } catch (error) {
+  //     console.error('유저 수정 실패:', error);
+  //     alert('유저 수정에 실패했습니다.');
+  //   }
+  // };
+  // // 전체 수정 함수(정상작동 확인))
+  // const handleBulkEdit = async () => {
+  //   try {
+  //     const confirmMessage = '수정사항을 반영하시겠습니까?';
+  //     const confirmed = window.confirm(confirmMessage);
+  //     if (!confirmed) return;
+  //     const bulkUserData = []; // 수정된 데이터를 담을 배열을 초기화
+  //     // 선택된 모든 사용자의 수정된 데이터를 추출하여 배열에 추가합니다.
+  //     checkedItems.forEach((checkedID) => {
+  //       const editedUserData = matchedData.find((user) => user.users_id === checkedID);
+  //       bulkUserData.push(editedUserData);
+  //     });
+  //     await editMutation(bulkUserData); // 수정된 사용자 데이터 서버로 전송
+  //   } catch (error) {
+  //     console.error('전체 수정 실패:', error);
+  //     alert('전체 수정에 실패했습니다.');
+  //   }
+  // };
+  // const handleEditSuccess = (data) => { // 수정 성공
+  //   console.log('수정 성공:', data);
+  //   alert('수정이 완료되었습니다.');
+  //   queryClient.invalidateQueries('holdusers');
+  //   setEditIndex(null);
+  // };
+  // const handleEditError = (error) => { // 수정 실패
+  //   console.error('수정 실패:', error);
+  //   alert('수정에 실패했습니다.');
+  // };
+  // const fetchEditUser = async (userData) => { // Fetching
+  //   try {
+  //     const response = await axios.post('/auth/update', userData);
+  //     return response.data;
+  //   } catch (error) {
+  //     throw error;
+  //   }
+  // };
+  // const { mutate: editMutation } = useMutation({ // Mutation
+  //   mutationFn: fetchEditUser,
+  //   onSuccess: handleEditSuccess,
+  //   onError: handleEditError,
+  // });
+  // // ------------------------고객정보 업데이트 기능 끝------------------------ //
 
 
-  // ------------------------삭제 기능 시작------------------------ //
-  // 고객 삭제
-  const fetchDeleteUser = async (userID) => { // Fetching
-    try {
-      const response = await axios.delete(`/auth/delete/${userID}`,)
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  }
-  const handleDelete = async (userID) => { // 사용 부분
-    try {
-      const confirmMessage = '선택된 고객을 정말 삭제하시겠습니까?';
-      const confirmed = window.confirm(confirmMessage);
-      if (!confirmed) return;
 
-      await deleteMutation(userID);
-    } catch (error) {
-      handleDeleteError(error);
-    }
-  };
-  const handleDeleteSuccess = (data) => { // 성공 시
-    console.log('user Delete successfully:', data);
-    alert(data.message);
-    queryClient.invalidateQueries(['holdusers']);
-    window.location.reload();
-  };
-  const handleDeleteError = (error) => { // 실패 시
-    console.error('user Delete failed:', error);
-    alert(error.message);
-  };
-  const { mutate: deleteMutation } = useMutation({ // Mutation
-    mutationFn: fetchDeleteUser,
-    onSuccess: handleDeleteSuccess,
-    onError: handleDeleteError,
-  });
+  // // ------------------------삭제 기능 시작------------------------ //
+  // // 고객 삭제
+  // const fetchDeleteUser = async (userID) => { // Fetching
+  //   try {
+  //     const response = await axios.delete(`/auth/delete/${userID}`,)
+  //     return response.data;
+  //   } catch (error) {
+  //     throw error;
+  //   }
+  // }
+  // const handleDelete = async (userID) => { // 사용 부분
+  //   try {
+  //     const confirmMessage = '선택된 고객을 정말 삭제하시겠습니까?';
+  //     const confirmed = window.confirm(confirmMessage);
+  //     if (!confirmed) return;
+
+  //     await deleteMutation(userID);
+  //   } catch (error) {
+  //     handleDeleteError(error);
+  //   }
+  // };
+  // const handleDeleteSuccess = (data) => { // 성공 시
+  //   console.log('user Delete successfully:', data);
+  //   alert(data.message);
+  //   queryClient.invalidateQueries(['holdusers']);
+  //   window.location.reload();
+  // };
+  // const handleDeleteError = (error) => { // 실패 시
+  //   console.error('user Delete failed:', error);
+  //   alert(error.message);
+  // };
+  // const { mutate: deleteMutation } = useMutation({ // Mutation
+  //   mutationFn: fetchDeleteUser,
+  //   onSuccess: handleDeleteSuccess,
+  //   onError: handleDeleteError,
+  // });
+  // // ------------------------삭제 기능 끝------------------------ //
 
   // 전체 체크박스 업데이트
   function handleAllCheckbox(isChecked) {
@@ -221,8 +227,8 @@ export default function useManagerUser() {
     matchedData, setMatchedData,
     handleToggleEdit,
     initializingData,
-    handleEdit, handleBulkEdit,
-    handleDelete,
+    // handleEdit, handleBulkEdit,
+    // handleDelete,
     handleAllCheckbox, handlePerCheckbox,
     updateValue,
     parseOptionValue,
