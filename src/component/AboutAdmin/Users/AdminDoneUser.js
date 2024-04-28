dd518f6d47171e8a4c5e70e79068931e31e00381
jsum01 <djadjawltjr@gmail.com>
2024-04-28 16:58:59 +0900

    ReFactor: Hold/Done User Component Management

--- src/component/AboutAdmin/Users/AdminDoneUser.js
+++ src/component/AboutAdmin/Users/AdminDoneUser.js
@@ -1,261 +1,27 @@
-import styles from './AdminUser.module.css';
-import React, { useEffect } from 'react';
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import AdminUserFilter from './AdminUserFilter';
-import AdminUserSort from './AdminUserSort';
-import { useModalActions, useModalState, usePageAction, usePageState, useUserFilter, useUserSort } from '../../../store/DataStore';
-import useManagerUser from './customFn/useManageUser';
-import { useFetch } from '../../../customFn/useFetch';
+import { useParsing } from '../../../customFn/useParsing';
 import UserDetailInfo from './UserDetailInfo';
-import axios from '../../../axios';
+import styles from "./UserList/UserList.module.css";
+import React from 'react';
+import { useModalState, useModalActions, useDevideType, usePageState, usePageAction, useCheckStoreState, useCheckStoreActions } from "../../../store/DataStore";
 
 
-export default function AdminDoneUser() {
 
-  const {
-    fetchServer, // 사용처 : 필터링, 정렬 API
-    fetchGetServer // 사용처 : 유저정보요청 API
-  } = useFetch();
+export default function AdminDoneUser({ matchedData, handleBulkEdit, handleEdit, handleToggleEdit, handleDelete, editIndex, setEditIndex, updateValue, initializingData }) {
 
-  const { itemsPerPage, currentPage, totalPages } = usePageState();
 
-  const { setItemsPerPage, setCurrentPage, setTotalPages } = usePageAction();
-
+  const { devideType } = useDevideType();
+  
+  const { itemsPerPage } = usePageState();
+  const { setItemsPerPage } = usePageAction();
   const { isModal, modalName, selectedIndex } = useModalState()
   const { selectedModalOpen, setSelectedIndex } = useModalActions();
-
-  const queryClient = useQueryClient(); // 리액트 쿼리 클라이언트
-
-  // UserManageFunctions Store :: 고객관리에 필요한 함수 스토어
-  const {
-    editIndex, setEditIndex,
-    checkedItems, setCheckedItems,
-    matchedData, setMatchedData,
-    handleToggleEdit,
-    initializingData,
-    handleAllCheckbox, handlePerCheckbox,
-    updateValue,
-    parseOptionValue
-  } = useManagerUser();
-
-  // esc키를 누르면 모달창 닫기.
-  useEffect(() => {
-    const exit_esc = (event) => {
-      if (event.key === 'Escape') {
-        initializingData();
-      }
-    };
-
-    window.addEventListener('keydown', exit_esc);
-
-    return () => {
-      window.removeEventListener('keydown', exit_esc);
-    };
-  }, [initializingData]);
-
-  // ------------------------------Fetch------------------------------ //
-
-
-
-  // 유저 데이터 Fetch
-  const fetchUsersData = async () => {
-    try {
-      const data = await fetchGetServer('/auth/read/done', currentPage);
-      return data.data;
-    } catch (error) {
-      throw new Error('고객 정보를 불러오는 중 오류가 발생하였습니다.');
-    }
-  }
-  const { isLoading, isError, error, data: userData } = useQuery({
-    queryKey: [`doneusers`, currentPage, itemsPerPage], // currentPage, itemPerPage가 변경될 때마다 재실행하기 위함
-    queryFn: fetchUsersData,
-  });
-
-  useEffect(() => {
-    if (userData)
-      setMatchedData(userData);
-  }, [userData, itemsPerPage, currentPage]);
-
-
-  console.log(userData);
-
-
-
-  // ------------------------고객정보 업데이트 기능 시작------------------------ //
-  // 고객 단일 수정
-  const handleEdit = async (userData) => { // 
-    try {
-      const confirmMessage = '수정사항을 반영하시겠습니까?';
-      const confirmed = window.confirm(confirmMessage);
-      if (!confirmed) return;
-
-      await editMutation(userData);
-      window.location.reload();
-    } catch (error) {
-      console.error('유저 수정 실패:', error);
-      alert('유저 수정에 실패했습니다.');
-    }
-  };
-  // 전체 수정 함수(정상작동 확인))
-  const handleBulkEdit = async () => {
-    try {
-      const confirmMessage = '수정사항을 반영하시겠습니까?';
-      const confirmed = window.confirm(confirmMessage);
-      if (!confirmed) return;
-      const bulkUserData = []; // 수정된 데이터를 담을 배열을 초기화
-      // 선택된 모든 사용자의 수정된 데이터를 추출하여 배열에 추가합니다.
-      checkedItems.forEach((checkedID) => {
-        const editedUserData = matchedData.find((user) => user.users_id === checkedID);
-        bulkUserData.push(editedUserData);
-      });
-      await editMutation(bulkUserData); // 수정된 사용자 데이터 서버로 전송
-    } catch (error) {
-      console.error('전체 수정 실패:', error);
-      alert('전체 수정에 실패했습니다.');
-    }
-  };
-  const handleEditSuccess = (data) => { // 수정 성공
-    console.log('수정 성공:', data);
-    alert('수정이 완료되었습니다.');
-    queryClient.invalidateQueries('doneusers');
-    setEditIndex(null);
-  };
-  const handleEditError = (error) => { // 수정 실패
-    console.error('수정 실패:', error);
-    alert('수정에 실패했습니다.');
-  };
-  const fetchEditUser = async (userData) => { // Fetching
-    try {
-      const response = await axios.post('/auth/update', userData);
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  };
-  const { mutate: editMutation } = useMutation({ // Mutation
-    mutationFn: fetchEditUser,
-    onSuccess: handleEditSuccess,
-    onError: handleEditError,
-  });
-  // ------------------------고객정보 업데이트 기능 끝------------------------ //
-
-
-
-  // ------------------------삭제 기능 시작------------------------ //
-  // 고객 삭제
-  const fetchDeleteUser = async (userID) => { // Fetching
-    try {
-      const response = await axios.delete(`/auth/delete/${userID}`,)
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  }
-  const handleDelete = async (userID) => { // 사용 부분
-    try {
-      const confirmMessage = '선택된 고객을 정말 삭제하시겠습니까?';
-      const confirmed = window.confirm(confirmMessage);
-      if (!confirmed) return;
-
-      await deleteMutation(userID);
-    } catch (error) {
-      handleDeleteError(error);
-    }
-  };
-  const handleDeleteSuccess = (data) => { // 성공 시
-    console.log('user Delete successfully:', data);
-    alert(data.message);
-    queryClient.invalidateQueries(['doneusers']);
-    window.location.reload();
-  };
-  const handleDeleteError = (error) => { // 실패 시
-    console.error('user Delete failed:', error);
-    alert(error.message);
-  };
-  const { mutate: deleteMutation } = useMutation({ // Mutation
-    mutationFn: fetchDeleteUser,
-    onSuccess: handleDeleteSuccess,
-    onError: handleDeleteError,
-  });
-  // ------------------------삭제 기능 끝------------------------ //
-
-
-
-
-  // 필터링 데이터 Fetch
-  const userFilter = useUserFilter();
-  const fetchFilteredDoneUser = async (userFilter) => {
-    console.log(userFilter);
-    return await fetchServer(userFilter, `post`, `/auth/filtering/done`, currentPage);
-  }
-  const { mutate: filteringMutation } = useMutation({ mutationFn: fetchFilteredDoneUser });
-  const onDoneFiltering = (userFilter) => {
-    console.log(`전달할 때 값: ${userFilter.cor_ceoName}`)
-    filteringMutation(userFilter, {
-      onSuccess: (data) => {
-        console.log('고객 필터링이 성공적으로 완료되었습니다.\n', data.data.data);
-        alert(data.message);
-        setCurrentPage(data.data.currentPage);
-        setTotalPages(data.data.totalPages);
-        queryClient.setQueryData(['doneusers', currentPage, itemsPerPage], () => {
-          return data.data.data;
-        })
-      },
-      onError: (error) => {
-        console.error('필터링에 실패했습니다.\n', error);
-        // 에러 처리 또는 메시지 표시
-        alert(error.message);
-      },
-    });
-  };
-
-
-
-  // 정렬 데이터 Fetch
-  const userSort = useUserSort();
-  const fetchSortedDoneUser = async (userSort) => {
-    // console.log(`선택된 정렬: ${userSort}`);
-    console.log(userSort);
-    return await fetchServer(userSort, `post`, `/auth/sorting/done`, currentPage);
-  };
-  const { mutate: sortingMutation } = useMutation({ mutationFn: fetchSortedDoneUser }); // 정렬
-  const handleDoneSorting = () => {
-    sortingMutation(userSort, {
-      onSuccess: (data) => {
-        // Debuggig Code : data.data.data
-        console.log('고객 정렬이 성공적으로 완료되었습니다.\n', data.data.data);
-        alert(data.message);
-        setCurrentPage(data.data.currentPage);
-        setTotalPages(data.data.totalPages);
-        // 다른 로직 수행 또는 상태 업데이트
-        queryClient.setQueryData(['doneusers', currentPage, itemsPerPage], () => {
-          return data.data.data
-        })
-      },
-      onError: (error) => {
-        console.error('user Sorted failed:', error);
-        // 에러 처리 또는 메시지 표시
-        alert(error);
-      },
-    });
-  };
-
-
-  if (isLoading) {
-    return <div>로딩 중...</div>;
-  }
-
-  if (isError) {
-    return <div>오류가 발생했습니다.</div>;
-  }
+  const { checkedItems } = useCheckStoreState();
+  const { setCheckedItems, handleEachItem, handleBatchItems } = useCheckStoreActions();
+  const { parseUserType, parseCMS } = useParsing();
 
 
   return (
-    <div className={styles.mainContainer}>
-      <div className={styles.filtSortContainer}>
-        <AdminUserFilter onFiltering={onDoneFiltering} />
-        <AdminUserSort onSort={handleDoneSorting} />
-      </div>
+    <div className={styles.listView}>
       {/* Header */}
       <div className='MediumHeader'>
         <div className='HeaderTxt'>
@@ -281,19 +47,22 @@ export default function AdminDoneUser() {
               <input
                 type='checkbox'
                 checked={checkedItems.length === matchedData?.length ? true : false}
-                onChange={(e) => handleAllCheckbox(e.target.checked)}
+                onChange={(e) => handleBatchItems(e.target.checked)}
               />
             </th>
             {/* 업체명(상호명) */}
             <th>업체명(상호명)</th>
             {/* 고객 구분, CMS여부 */}
             {[
-              { title: '고객 구분', valList: [1, 2, 12, 13, 14, 22, 23, 24, 100], val: userData.userType_id, key: 'userType_id' },
-              { title: '담당자', valList: ['엄지석'], val: userData.managerName, key: 'managerName' },
-              { title: 'CMS여부', valList: [1, 0], val: userData.hasCMS, key: 'hasCMS' },
+              { title: '고객 구분', valList: [1, 2, 12, 13, 14, 22, 23, 24, 100], val: matchedData ? matchedData.userType_id : "요청실패", key: 'userType_id' },
+              { title: '담당자', valList: ['엄지석'], val: matchedData ? matchedData.managerName : "요청실패", key: 'managerName' },
+              { title: 'CMS여부', valList: [1, 0], val: matchedData ? matchedData.hasCMS : "요청실패", key: 'hasCMS' },
             ].map((customItem, index) => (
               <th key={index}>
-                {editIndex === 'allEdit' ?
+                {editIndex !== 'allEdit'
+                  ?
+                  customItem.title
+                  :
                   <>
                     <span>{customItem.title}</span>
                     <select
@@ -304,13 +73,13 @@ export default function AdminDoneUser() {
                       <option value={null}>선택</option>
                       {customItem.valList.map((valListItem, valListIndex) => (
                         <option key={valListIndex} value={valListItem}>
-                          {parseOptionValue(customItem.key, valListItem)}
+                          {customItem.key == 'userType_id' && parseUserType(valListItem)}
+                          {customItem.key == 'hasCMS' && parseUserType(valListItem)}
+                          {customItem.key == 'managerName' && customItem.key}
                         </option>
                       ))}
                     </select>
                   </>
-                  :
-                  customItem.title
                 }
               </th>
             ))}
@@ -355,14 +124,14 @@ export default function AdminDoneUser() {
                 <input
                   type='checkbox'
                   checked={checkedItems.includes(user.users_id)}
-                  onChange={(e) => handlePerCheckbox(e.target.checked, user.users_id)}
+                  onChange={(e) => handleEachItem(e.target.checked, user.users_id)}
                 />
               </td>
               {/* 업체명(상호명) : name */}
               <td onClick={() => {
                 // 수정상태가 활성화되지 않은 상태에서만 모달이 작동하도록 합니다.
                 if (editIndex != index) {
-                  selectedModalOpen('doneuser'); // modalName 설정
+                  selectedModalOpen(`${devideType}user`); // modalName 설정
                   setSelectedIndex(index); // index 동기화
                 }
               }}>
@@ -382,16 +151,22 @@ export default function AdminDoneUser() {
                       onChange={e => updateValue(e, customItem.key, index)}
                     >
                       <option value={customItem.val}>
-                        {parseOptionValue(customItem.key, customItem.val)}
+                        {customItem.key == 'userType_id' && parseUserType(customItem.val)}
+                        {customItem.key == 'hasCMS' && parseUserType(customItem.val)}
+                        {customItem.key == 'managerName' && customItem.val}
                       </option>
                       {customItem.valList.map((item, index) => (
                         <option key={index} value={customItem.val}>
-                          {parseOptionValue(customItem.key, item)}
+                          {customItem.key == 'userType_id' && parseUserType(customItem.val)}
+                          {customItem.key == 'hasCMS' && parseUserType(customItem.val)}
+                          {customItem.key == 'managerName' && customItem.val}
                         </option>
                       ))}
                     </select>
                     :
-                    parseOptionValue(customItem.key, customItem.val)
+                    customItem.key === 'userType_id' ? parseUserType(customItem.val) : (
+                      customItem.key === 'hasCMS' ? parseUserType(customItem.val) : customItem.val
+                    )
                   }
                 </td>
               ))}
---
+++ src/component/AboutAdmin/Users/UserList/UserList.js
@@ -0,0 +1,366 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+  useUserFilter, useUserSort,
+  useCheckStoreState, useCheckStoreActions,
+  useDevideType,
+  usePageState, usePageAction
+} from "../../../../store/DataStore";
+import AdminDoneUser from "../AdminDoneUser";
+import AdminHoldUser from "../AdminHoldUser";
+import AdminUserFilter from "../AdminUserFilter";
+import AdminUserSort from "../AdminUserSort";
+import styles from "./UserList.module.css";
+import { useFetch } from "../../../../customFn/useFetch";
+import axios from "../../../../axios";
+import { useState, useEffect } from "react";
+
+const UserList = () => {
+
+  const { devideType } = useDevideType();
+  const { fetchServer, fetchGetServer } = useFetch();
+  const { itemsPerPage, currentPage } = usePageState();
+  const { setCurrentPage, setTotalPages } = usePageAction();
+  const { checkedItems } = useCheckStoreState();
+  const { setCheckedItems } = useCheckStoreActions();
+
+  const queryClient = useQueryClient(); // 리액트 쿼리 클라이언트
+  const [matchedData, setMatchedData] = useState([]);
+  const [editIndex, setEditIndex] = useState(-1);// -1은 선택되지 않았다는 의미
+
+
+
+  // ------------------------------userData Fetch------------------------------ //
+
+  // 유저 데이터 Fetch
+  const fetchUsersData = async () => {
+    console.log(`요청 타입: ${devideType}`);
+    try {
+      const data = await fetchGetServer(`/auth/read/${devideType}`, currentPage);
+      return data.data;
+    } catch (error) {
+      throw new Error('고객 정보를 불러오는 중 오류가 발생하였습니다.');
+    }
+  }
+  // useQuery]
+  const { isLoading, isError, error, data: userData } = useQuery({
+    queryKey: [`users`, devideType, currentPage, itemsPerPage], // currentPage, itemPerPage가 변경될 때마다 재실행하기 위함
+    queryFn: fetchUsersData,
+  });
+
+  // matchedData에 요청받은 userData저장
+  useEffect(() => {
+    if (userData)
+      setMatchedData(userData);
+  }, [userData, itemsPerPage, currentPage]);
+
+  console.log(`응답받은 고객 데이터: ${userData}`); // Debugging Code
+
+  // ------------------------------userData Fetch------------------------------ //
+
+
+  // ------------------------고객정보 업데이트------------------------ //
+
+  const handleEdit = async (userData) => { // 고객 단일 수정
+    try {
+      const confirmMessage = '수정사항을 반영하시겠습니까?';
+      const confirmed = window.confirm(confirmMessage);
+      if (!confirmed) return;
+
+      await editMutation(userData);
+      window.location.reload();
+    } catch (error) {
+      console.error('유저 수정 실패:', error);
+      alert('유저 수정에 실패했습니다.');
+    }
+  };
+
+  const handleBulkEdit = async () => {// 전체 수정 함수(정상작동 확인))
+    try {
+      const confirmMessage = '수정사항을 반영하시겠습니까?';
+      const confirmed = window.confirm(confirmMessage);
+      if (!confirmed) return;
+      const bulkUserData = []; // 수정된 데이터를 담을 배열을 초기화
+      // 선택된 모든 사용자의 수정된 데이터를 추출하여 배열에 추가합니다.
+      checkedItems.forEach((checkedID) => {
+        const editedUserData = setMatchedData.find((user) => user.users_id === checkedID);
+        bulkUserData.push(editedUserData);
+      });
+      await editMutation(bulkUserData); // 수정된 사용자 데이터 서버로 전송
+    } catch (error) {
+      console.error('전체 수정 실패:', error);
+      alert('전체 수정에 실패했습니다.');
+    }
+  };
+
+  const handleEditSuccess = (data) => { // 수정 성공
+    console.log('수정 성공:', data);
+    alert('수정이 완료되었습니다.');
+    queryClient.invalidateQueries('users');
+    setEditIndex(null);
+  };
+
+  const handleEditError = (error) => { // 수정 실패
+    console.error('수정 실패:', error);
+    alert('수정에 실패했습니다.');
+  };
+
+  const fetchEditUser = async (userData) => { // Fetching
+    try {
+      const response = await axios.post('/auth/update', userData);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const { mutate: editMutation } = useMutation({ // Mutation
+    mutationFn: fetchEditUser,
+    onSuccess: handleEditSuccess,
+    onError: handleEditError,
+  });
+
+  // ------------------------고객정보 업데이트 기능 끝------------------------ //
+
+
+
+  // ------------------------삭제 기능 시작------------------------ //
+
+  const fetchDeleteUser = async (userID) => { // 고객 삭제
+    try {
+      const response = await axios.delete(`/auth/delete/${userID}`,)
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  const handleDelete = async (userID) => { // 사용 부분
+    try {
+      const confirmMessage = '선택된 고객을 정말 삭제하시겠습니까?';
+      const confirmed = window.confirm(confirmMessage);
+      if (!confirmed) return;
+
+      await deleteMutation(userID);
+    } catch (error) {
+      handleDeleteError(error);
+    }
+  };
+
+  const handleDeleteSuccess = (data) => { // 성공 시
+    console.log('user Delete successfully:', data);
+    alert(data.message);
+    queryClient.invalidateQueries(['users']);
+    window.location.reload();
+  };
+
+  const handleDeleteError = (error) => { // 실패 시
+    console.error('user Delete failed:', error);
+    alert(error.message);
+  };
+
+  const { mutate: deleteMutation } = useMutation({ // Mutation
+    mutationFn: fetchDeleteUser,
+    onSuccess: handleDeleteSuccess,
+    onError: handleDeleteError,
+  });
+
+  // ------------------------삭제 기능 끝------------------------ //
+
+
+
+
+  // ------------------------필터링 기능 시작------------------------ //
+
+  const userFilter = useUserFilter();
+
+  const fetchFilteredUser = async (userFilter) => {
+    console.log(userFilter);
+    return await fetchServer(userFilter, `post`, `/auth/filtering/${devideType}`, currentPage);
+  }
+
+  const { mutate: filteringMutation } = useMutation({ mutationFn: fetchFilteredUser });
+
+  const onFiltering = (userFilter) => {
+    console.log(`전달할 때 값: ${userFilter.cor_ceoName}`)
+    filteringMutation(userFilter, {
+      onSuccess: (data) => {
+        console.log('고객 필터링이 성공적으로 완료되었습니다.\n', data.data.data);
+        alert(data.message);
+        setCurrentPage(data.data.currentPage);
+        setTotalPages(data.data.totalPages);
+        queryClient.setQueryData([`users`, currentPage, itemsPerPage], () => {
+          return data.data.data;
+        })
+      },
+      onError: (error) => {
+        console.error('필터링에 실패했습니다.\n', error);
+        // 에러 처리 또는 메시지 표시
+        alert(error.message);
+      },
+    });
+  };
+
+  // ------------------------필터링 기능 끝------------------------ //
+
+
+
+  // ------------------------정렬 기능 시작------------------------ //
+
+  const userSort = useUserSort();
+
+  const fetchSortedUser = async (userSort) => {
+
+    // console.log(`선택된 정렬: ${userSort}`);
+    console.log(userSort);
+    return await fetchServer(userSort, `post`, `/auth/sorting/${devideType}`, currentPage);
+  };
+
+  const { mutate: sortingMutation } = useMutation({ mutationFn: fetchSortedUser }); // 정렬
+
+  const handleSorting = () => {
+    sortingMutation(userSort, {
+      onSuccess: (data) => {
+        // Debuggig Code : data.data.data
+        console.log('고객 정렬이 성공적으로 완료되었습니다.\n', data.data.data);
+        alert(data.message);
+        setCurrentPage(data.data.currentPage);
+        setTotalPages(data.data.totalPages);
+        // 다른 로직 수행 또는 상태 업데이트
+        queryClient.setQueryData([`users`, currentPage, itemsPerPage], () => {
+          return data.data.data
+        })
+      },
+      onError: (error) => {
+        console.error('user Sorted failed:', error);
+        // 에러 처리 또는 메시지 표시
+        alert(error);
+      },
+    });
+  };
+
+  // ------------------------정렬 기능------------------------ //
+
+
+
+  // ------------------------------해당 인덱스 수정기능 토글------------------------------ //
+
+  // handleToggleEdit 함수를 수정하여 editIndex를 배열에서 단일 값으로 업데이트합니다.
+  const handleToggleEdit = (index) => {
+    if (editIndex === index) {
+      setEditIndex(null);
+    } else {
+      setEditIndex(index);
+    }
+  };
+
+  // 편집 상태 & 입력된 정보 & 체크리스트 초기화
+  const initializingData = () => {
+    if (editIndex !== null) {
+      setEditIndex(null);
+      setCheckedItems([]);
+    }
+  }
+
+  // ------------------------------해당 인덱스 수정기능 토글------------------------------ //
+
+
+
+  // ------------------------------변경사항 업데이트------------------------------ //
+
+  const updateValue = (e, itemKey, itemIndex) => {
+    console.log(checkedItems.length);
+
+    // 여러 항목 업데이트
+    if (checkedItems.length > 0) {
+      const editData = e.target.value;
+      const updateData = matchedData.map((item) => {
+        // checkedItems에 포함된 사용자의 id와 일치하는 사용자의 데이터만 업데이트
+        if (checkedItems.includes(item.users_id)) {
+          return { ...item, [itemKey]: editData };
+        }
+        return item;
+      });
+      setMatchedData(updateData);
+    }
+    // 단일 항목 업데이트
+    else {
+      const editData = e.target.value;
+      const newData = matchedData.map((item, idx) => {
+        if (idx === itemIndex) {
+          return { ...item, [itemKey]: editData };
+        }
+        return item;
+      });
+      setMatchedData(newData);
+    }
+  };
+
+  // ------------------------------변경사항 업데이트------------------------------ //
+
+
+
+  // ------------------------------모달 keyDown event------------------------------ //
+
+  useEffect(() => {
+    const exit_esc = (event) => {
+      if (event.key === 'Escape') {
+        initializingData();
+      }
+    };
+
+    window.addEventListener('keydown', exit_esc);
+
+    return () => {
+      window.removeEventListener('keydown', exit_esc);
+    };
+  }, [initializingData]);
+
+  // ------------------------------모달 keyDown event------------------------------ //
+
+
+
+  // ------------------------페이지 로딩 Section------------------------ //
+
+  if (isLoading) return <div>로딩 중...</div>;
+  if (isError) return <div>오류가 발생했습니다.</div>;
+
+  // ------------------------페이지 로딩 Section------------------------ //
+
+  return (
+    <div className={styles.body}>
+      {/* Flex Container */}
+      <div className={styles.flexContainer}>
+        {/* 필터링 */}
+        <AdminUserFilter />
+        {/* 정렬 */}
+        <AdminUserSort />
+      </div>
+
+      {
+        devideType == 'done'
+          ?
+          <AdminHoldUser
+            matchedData={matchedData}
+            handleBulkEdit={handleBulkEdit}
+            handleEdit={handleEdit}
+            handleToggleEdit={handleToggleEdit}
+            handleDelete={handleDelete}
+            editInde={editIndex} setEditIndex={setEditIndex}
+            updateValue={updateValue}
+            initializingData={initializingData} />
+          :
+          <AdminDoneUser
+            matchedData={matchedData}
+            handleBulkEdit={handleBulkEdit}
+            handleEdit={handleEdit}
+            handleToggleEdit={handleToggleEdit}
+            handleDelete={handleDelete}
+            editInde={editIndex} setEditIndex={setEditIndex}
+            updateValue={updateValue}
+            initializingData={initializingData} />
+      }
+    </div>
+  )
+}
+
+export default UserList;
